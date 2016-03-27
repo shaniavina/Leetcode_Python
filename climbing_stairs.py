8c9e9f3680e730e8145f1fafcf1ac8bdd5091d1f
shaniavina <shaniavina@gmail.com>
2016-03-27 11:46:12 -0400

    Create climbing_stairs.py

---
+++ climbing_stairs.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        pre, cur = 1, 2
+        
+        for i in range(n - 1):
+            a = cur
+            cur += pre
+            pre = a
+        return pre
