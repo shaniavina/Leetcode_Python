fa2b90cad5a0c342fea95e2303713017329d2f2c
shaniavina <shaniavina@gmail.com>
2016-03-27 19:49:31 -0400

    Create implement_stack_using_queues.py

---
+++ implement_stack_using_queues.py
@@ -0,0 +1,31 @@
+class Stack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.A, self.B = [], []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.A.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        return self.A.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.A[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.A) == 0 and len(self.B) == 0
