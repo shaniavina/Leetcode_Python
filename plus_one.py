6b3ccc556fc6f56a8956002ff22ceeef666ff5e4
shaniavina <shaniavina@gmail.com>
2016-04-17 21:47:58 -0400

    Create plus_one.py

---
+++ plus_one.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        for i in reversed(range(len(digits))):
+            digits[i] += carry
+            carry = digits[i] / 10
+            digits[i] %= 10
+        if carry:
+            digits = [1] + digits
+        return digits
