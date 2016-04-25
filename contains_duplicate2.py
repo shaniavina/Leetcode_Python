7bcf42ed9d1bdc6658d638ba3fa5d8ad9d817521
shaniavina <shaniavina@gmail.com>
2016-04-25 17:18:29 -0400

    Create contains_duplicate2.py

---
+++ contains_duplicate2.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        num_pos_map = {}
+        for i in range(len(nums)):
+            if (nums[i] in num_pos_map) and (i - num_pos_map[nums[i]] <= k):
+                return True
+            else:
+                num_pos_map[nums[i]] = i
+            
+        
+        return False
+                
