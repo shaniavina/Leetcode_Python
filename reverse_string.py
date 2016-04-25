ee9be589e7ba1dc0e2bbc6e6839a45a6829619a3
shaniavina <shaniavina@gmail.com>
2016-04-25 17:14:27 -0400

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = [0] * (len(s))
+        j = len(s)
+        for i in range(len(s)):
+            j -= 1
+            res[j] = s[i]
+            
+        return "".join(res)
