be3bf293f5913a74121f5d4a361fbe5b8f356c39
shaniavina <shaniavina@gmail.com>
2016-05-23 23:08:15 -0400

    Create intersection_of_two_arrays2.py

---
+++ intersection_of_two_arrays2.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort(), nums2.sort()
+        res = []
+        it1, it2 = 0, 0
+        
+        while it1 < len(nums1) and it2 < len(nums2):
+            if nums1[it1] < nums2[it2]:
+                it1 += 1
+            elif nums1[it1] > nums2[it2]:
+                it2 += 1
+            else:
+                res.append(nums1[it1])
+                it1 += 1
+                it2 +=1
+        return res
