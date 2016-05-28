cd9833560133795511d1eb6557c3ba9a8395e6d5
shaniavina <shaniavina@gmail.com>
2016-05-27 23:00:32 -0400

    Create valid_parentheses.py

---
+++ valid_parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        goodPair = {"(":")","[":"]","{":"}"}
+        for l in s:
+            if l == "(" or l == "[" or l == "{":
+                stack.append(l)
+            else:
+                if len(stack) == 0:
+                    return False
+                else:
+                    b = stack.pop()
+                    if goodPair[b] != l:
+                        return False
+        return len(stack) == 0
