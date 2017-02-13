dc846c25c324610238b31707860b2a4eb039e587
shaniavina <shaniavina@gmail.com>
2017-02-12 23:05:17 -0500

    Update add_digits.py

--- add_digits.py
+++ add_digits.py
@@ -5,3 +5,12 @@ class Solution(object):
         :rtype: int
         """
         return (num - 1) % 9 + 1 if num >0 else 0
+
+    ###second solution
+    
+     if num == 0:
+            return 0
+        elif num % 9 == 0:
+            return 9
+        else:
+            return num % 9
