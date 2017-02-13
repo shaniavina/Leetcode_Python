505ff57bf1eb1ef302b7841f83db7def12a0a372
shaniavina <shaniavina@gmail.com>
2017-02-12 21:20:56 -0500

    Create find_the_difference.py

---
+++ find_the_difference.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        s_map = {}
+        for l in s:
+            if l in s_map:
+                s_map[l] += 1
+            else:
+                s_map[l] = 1
+
+        t_map = {}
+        for l in t:
+            if l in t_map:
+                t_map[l] += 1
+            else:
+                t_map[l] = 1
+                
+        for l in t_map:
+            if t_map[l] != s_map.get(l, 0):
+                return l
+                
+        
+          ###second solution 
+    def findTheDifference2(self, s, t):
+         s_sorted = sorted(s)
+        t_sorted = sorted(t)
+        
+        for i in range(len(s)):
+            if s_sorted[i] != t_sorted[i]:
+                return t_sorted[i]
+        return t_sorted[-1]
