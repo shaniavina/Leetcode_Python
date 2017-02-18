168cd6dcd83bed7c3304e5ec5ab403f8301d7cf5
shaniavina <shaniavina@gmail.com>
2017-02-18 12:50:01 -0500

    Update and rename majority_element to majority_element.py

--- majority_element.py
+++ majority_element.py
@@ -18,3 +18,21 @@ class Solution(object):
 if __name__ =="__main__":
     print(Solution().majorityElement([1,2,2,2,2,2,2,2,2,2,3,4,5]))
                     
+#####second solution
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = {}
+        for i in range(len(nums)):
+            if nums[i] in count:
+                count[nums[i]] += 1
+            else:
+                count[nums[i]] = 1
+        for key in count:
+            if count[key] >= (len(nums) + 1) / 2:
+                return key
+      
