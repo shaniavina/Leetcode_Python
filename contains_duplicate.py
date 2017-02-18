2ee365b6870a8cc9bcbc8bc004c6e97582d43a74
shaniavina <shaniavina@gmail.com>
2017-02-18 13:29:25 -0500

    Update and rename contains_duplicate to contains_duplicate.py

---
+++ contains_duplicate.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) > len(set(nums))
+
+###second solution
+
+
+        count = {}
+        
+        for num in nums:
+            if num in count:
+                return True;
+            else:
+                count[num] = 1
+        return False
+            
