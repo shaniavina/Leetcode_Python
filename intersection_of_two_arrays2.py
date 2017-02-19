5df71b1e7e8aede61ba8be89b267410ce8681a82
shaniavina <shaniavina@gmail.com>
2017-02-18 22:16:37 -0500

    Update intersection_of_two_arrays2.py

--- intersection_of_two_arrays2.py
+++ intersection_of_two_arrays2.py
@@ -19,3 +19,32 @@ class Solution(object):
                 it1 += 1
                 it2 +=1
         return res
+
+    ##########second
+    class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        count1 = {}
+        count2 = {}
+        res = []
+        for i in range(len(nums1)):
+            if nums1[i] in count1:
+                count1[nums1[i]] += 1
+            else:
+                count1[nums1[i]] = 1
+                
+        for i in range(len(nums2)):
+            if nums2[i] in count2:
+                count2[nums2[i]] += 1
+            else:
+                count2[nums2[i]] = 1
+        for key1 in count1:
+            for key2 in count2:
+                if key1 == key2:
+                    min_count = min(count1[key1], count2[key2])
+                    res.extend([key1] * min_count)
+        return res
