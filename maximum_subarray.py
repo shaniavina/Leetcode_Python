fe0e406fcd7b87298b283ecf51651b84851e5e43
shaniavina <shaniavina@gmail.com>
2017-02-19 18:30:10 -0500

    Create maximum_subarray.py

---
+++ maximum_subarray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if max(nums) < 0:
+            return max(nums)
+        global_max, local_max = float("-inf"), 0
+        for x in nums:
+            local_max = max(0, local_max + x)
+            global_max = max(global_max, local_max)
+        return global_max
