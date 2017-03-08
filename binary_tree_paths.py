604170fef7f70750ce4b4c1ea8b62b4fdc193747
shaniavina <shaniavina@gmail.com>
2017-03-08 09:55:30 -0500

    Create binary_tree_paths.py

---
+++ binary_tree_paths.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        result, path = [], []
+        self.binaryTreePathsRecu(root, path, result)
+        return result
+    
+    def binaryTreePathsRecu(self, node, path, result):
+        if node is None:
+            return
+
+        if node.left is None and node.right is None:
+            ans = ""
+            for n in path:
+                ans += str(n.val) + "->"
+            result.append(ans + str(node.val))
+
+        if node.left:
+            path.append(node)
+            self.binaryTreePathsRecu(node.left, path, result)
+            path.pop()
+
+        if node.right:
+            path.append(node)
+            self.binaryTreePathsRecu(node.right, path, result)
+            path.pop()
