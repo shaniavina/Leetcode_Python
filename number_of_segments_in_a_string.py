2504cb36c8af0fe1e6f914c3bb1a5c7b111ed49e
shaniavina <shaniavina@gmail.com>
2017-03-08 20:13:18 -0500

    Create number_of_segments_in_a_string.py

---
+++ number_of_segments_in_a_string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = int(len(s) and s[-1] != ' ')
+        for i in range(1, len(s)):
+            if s[i] == ' ' and s[i - 1] != ' ':
+                result += 1
+        return result
