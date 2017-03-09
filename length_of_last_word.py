90fde27743fbfefc4211b2b1ec91b4ce7df13ed1
shaniavina <shaniavina@gmail.com>
2017-03-08 20:50:41 -0500

    Update length_of_last_word.py

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         length = 0
-        for i in reversed(s):
+        for i in reversed(s):  ####s[::-1]
             if i == " ":
                 if length:
                     break
