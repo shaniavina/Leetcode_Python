6a0a3b9eaa97664d2c0bb419eb4e5bf8936ba0a7
shaniavina <shaniavina@gmail.com>
2017-03-14 23:50:20 -0400

    Update intersection_of_two_linked_lists.py

--- intersection_of_two_linked_lists.py
+++ intersection_of_two_linked_lists.py
@@ -45,3 +45,30 @@ class Solution(object):
                 curA = curA.next
                 curB = curB.next
         return curA
+    
+    
+    ############second solution
+    
+    # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+        
+        pa = headA
+        pb = headB
+
+        while pa != pb:
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next 
+        ###switch the head, at the second traversal, they either meet or hit the end
+        return pa
