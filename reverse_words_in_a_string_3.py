a84cdd17e9e063eaa157881a292d33af46d2509d
shaniavina <shaniavina@gmail.com>
2017-04-14 22:47:15 -0400

    Create reverse_words_in_a_string_3.py

---
+++ reverse_words_in_a_string_3.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        def reverse(s, begin, end):
+            for i in range((end - begin)/2):
+                s[begin + i], s[end - 1 - i] = s[end - 1 - i], s[begin + i]
+        
+        s, i = list(s), 0
+        for j in range(len(s) + 1):
+            if j == len(s) or s[j] == ' ':
+                reverse(s, i, j)
+                i = j + 1
+        return ''.join(s)
