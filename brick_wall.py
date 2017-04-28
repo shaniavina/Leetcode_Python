360f0cd48a8058e77126377ffda8925926c032b4
shaniavina <shaniavina@gmail.com>
2017-04-28 00:30:09 -0400

    Create brick_wall.py

---
+++ brick_wall.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def leastBricks(self, wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+        d = collections.defaultdict(int)
+        for line in wall:
+            i = 0
+            for brick in line[:-1]:
+                i += brick
+                d[i] += 1
+        if not d:
+            return len(wall)    ######not sure if d exists
+        return len(wall) - max(d.values())
