6fe87ac870144bdcbceec5197a4e67296701b1da
shaniavina <shaniavina@gmail.com>
2017-04-29 16:24:55 -0400

    Create insert_delete_getRandom_O1.py

---
+++ insert_delete_getRandom_O1.py
@@ -0,0 +1,51 @@
+from random import randint
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.__set = []
+        self.__used = {}
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.__used:
+            return False
+
+        self.__set += val,
+        self.__used[val] = len(self.__set)-1
+
+        return True
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.__used:
+            return False
+        self.__used[self.__set[-1]] = self.__used[val]
+        self.__set[self.__used[val]], self.__set[-1] = self.__set[-1], self.__set[self.__used[val]]
+        
+        self.__used.pop(val)
+        self.__set.pop()
+        return True
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return self.__set[randint(0, len(self.__set) - 1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
