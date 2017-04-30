80435287f259b3007a3edd15842c5f0de69820d9
shaniavina <shaniavina@gmail.com>
2017-04-30 16:13:48 -0400

    Create longest_increasing_subsequence.py

---
+++ longest_increasing_subsequence.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        LIS = []
+        def insert(target):
+            left, right = 0, len(LIS) - 1
+            # Find the first index "left" which satisfies LIS[left] >= target
+            while left <= right:
+                mid = left + (right - left) / 2;
+                if LIS[mid] >= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # If not found, append the target.
+            if left == len(LIS):
+                LIS.append(target)
+            else:
+                LIS[left] = target
+
+        
+        
+        for num in nums:
+            insert(num)
+        return len(LIS)
