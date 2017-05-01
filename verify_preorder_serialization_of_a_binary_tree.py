9acfe8440a23fd001ffe2fc07f57f537039988d5
shaniavina <shaniavina@gmail.com>
2017-04-30 23:59:55 -0400

    Create verify_preorder_serialization_of_a_binary_tree.py

---
+++ verify_preorder_serialization_of_a_binary_tree.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """
+        if not preorder:
+            return False
+        
+        depth, cnt = 0, preorder.count(',') + 1
+        for c in preorder.split(','):
+            cnt -= 1
+            if c == '#':
+                depth -= 1
+                if depth < 0:
+                    break
+            else:
+                depth += 1
+        return cnt == 0 and depth < 0
