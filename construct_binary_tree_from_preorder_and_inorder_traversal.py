27b31fdf292bc930bd6269f777026912e231b164
shaniavina <shaniavina@gmail.com>
2017-05-03 22:07:40 -0400

    Create construct_binary_tree_from_preorder_and_inorder_traversal.py

---
+++ construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        lookup = {}
+        for i, num in enumerate(inorder):
+            lookup[num] = i
+        return self.buildTreeRecu(lookup, preorder, inorder, 0, 0, len(inorder))
+    
+    def buildTreeRecu(self, lookup, preorder, inorder, pre_start, in_start, in_end):
+        if in_start == in_end:
+            return None
+        node = TreeNode(preorder[pre_start])
+        i = lookup[preorder[pre_start]]
+        node.left = self.buildTreeRecu(lookup, preorder, inorder, pre_start + 1, in_start, i)
+        node.right = self.buildTreeRecu(lookup, preorder, inorder, pre_start + 1 + i - in_start, i + 1, in_end)
+        return node
