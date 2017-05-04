2f2822dd2ee8f1130c82a2fd60158902f8eccd06
shaniavina <shaniavina@gmail.com>
2017-05-03 21:33:15 -0400

    Create course_schedule.py

---
+++ course_schedule.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        zero_in_degree_queue, in_degree, out_degree = collections.deque(), {}, {}
+        
+        for i, j in prerequisites:
+            if i not in in_degree:
+                in_degree[i] = set()
+            if j not in out_degree:
+                out_degree[j] = set()
+            in_degree[i].add(j)
+            out_degree[j].add(i)
+            
+        for i in range(numCourses):
+            if i not in in_degree:
+                zero_in_degree_queue.append(i)
+        
+        while zero_in_degree_queue:
+            prerequisite = zero_in_degree_queue.popleft()
+            if prerequisite in out_degree:
+                for course in out_degree[prerequisite]:
+                    in_degree[course].discard(prerequisite)
+                    if not in_degree[course]:
+                        zero_in_degree_queue.append(course)
+            
+                del out_degree[prerequisite]
+        
+        if out_degree:
+            return False
+        
+        return True
