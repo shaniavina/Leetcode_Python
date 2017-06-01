ae3152853d6c742e19e70206552f009c2dec6be9
shanshanli <shaniavina@gmail.com>
2017-06-01 17:56:45 -0400

    Update reverse_linked_list.py

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -3,48 +3,26 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-????????????
+
 class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        dummy = ListNode(float("-inf"))
+        ####my edition: iteratively
+        dummy = ListNode('-inf')
         while head:
-            dummy.next, head.next, head = head, dummy.next, head.next
+            head.next, dummy.next, head = dummy.next, head, head.next
+            
         return dummy.next
 
 # Time:  O(n)
 # Space: O(n)
 # Recursive solution.  
 
-
-##？？？？？？？？？？？？？？？？？？？
-class Solution2:
-    # @param {ListNode} head
-    # @return {ListNode}
-    def reverseList(self, head):
-        [begin, end] = self.reverseListRecu(head)
-        return begin
-    
-    def reverseListRecu(self, head):
-        if not head:
-            return [None, None]
-            
-        [begin, end] = self.reverseListRecu(head.next)
-        
-        if end:
-            end.next = head
-            head.next = None
-            return [begin, head]
-        else:
-            return [head, head]
-########??????????????
-
 class Solution:
-    # @param {ListNode} head
-    # @return {ListNode}
+    
     def reverseList(self, head):
         return self.doReverse(head, None)
     def doReverse(self, head, newHead):
