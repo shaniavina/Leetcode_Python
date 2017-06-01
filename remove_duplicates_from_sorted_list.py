e1ca9c25e9427d4af65ed289c5995ac9df499c7b
shanshanli <shaniavina@gmail.com>
2017-06-01 11:44:51 -0400

    Update remove_duplicates_from_sorted_list.py

--- remove_duplicates_from_sorted_list.py
+++ remove_duplicates_from_sorted_list.py
@@ -11,13 +11,13 @@ class Solution(object):
         :rtype: ListNode
         """
         if not head:
-            return head
+            return None
+        
         cur = head
-        while cur:
-            runner = cur.next
-            while runner and runner.val == cur.val:
-                runner = runner.next
-                cur.next = runner
-            cur = cur.next
-        return head
-                
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        
+        return head       
