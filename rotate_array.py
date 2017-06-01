e18c852c8a7b22b4bc8e194e55d9ceff62582554
shanshanli <shaniavina@gmail.com>
2017-05-31 22:07:23 -0400

    Update rotate_array.py

--- rotate_array.py
+++ rotate_array.py
@@ -5,6 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]  ####can't be written as:nums = nums[n-k:] + nums[:n-k]
+        k %= len(nums)
+        nums[:] = nums[len(nums) - k :] + nums[:len(nums) - k]  ####can't be written as:nums = nums[n-k:] + nums[:n-k]
 
+        
+        
+        @@@@@@second
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)   ####### k steps, you have to make sure it is less than len(nums)
+        
+        self.reverse(nums, 0, len(nums))
+        self.reverse(nums, 0, k)
+        self.reverse(nums, k, len(nums))
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end - 1] = nums[end - 1], nums[start]
+            start += 1
+            end -= 1
