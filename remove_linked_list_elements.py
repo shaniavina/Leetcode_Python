4a83d56878f8855207917fe234488d07df73c686
shanshanli <shaniavina@gmail.com>
2017-06-01 12:30:19 -0400

    Update remove_linked_list_elements.py

--- remove_linked_list_elements.py
+++ remove_linked_list_elements.py
@@ -22,3 +22,38 @@ class Solution(object):
                 pre = curr
             curr = curr.next
         return dummy.next
+    
+    
+@@@@@@@myself
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+            
+        dummy = ListNode('-inf')
+        pre = dummy
+        cur = head
+        
+        while cur:
+            if cur.val == val:
+                pre.next = cur.next
+               
+                cur = cur.next
+            else:
+                pre.next = cur
+                pre = cur
+                cur = cur.next
+        return dummy.next
+                
+                
