36b4384320abe54ff753720e2a0a36231b91ae7f
shanshanli <shaniavina@gmail.com>
2017-06-02 00:21:20 -0400

    Update hamming_distance.py

--- hamming_distance.py
+++ hamming_distance.py
@@ -11,3 +11,19 @@ class Solution(object):
             distance += 1
             z &= z - 1
         return distance
+    
+    #####my edition
+    class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        res = 0
+        xor = x ^ y
+        for i in range(32):
+            mask = 1 << i
+            if mask & (xor):
+                res += 1
+        return res
