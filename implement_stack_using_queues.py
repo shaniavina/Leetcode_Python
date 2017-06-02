48a76083dd02d4db5d7aeafd1b4f58ea5d81ae77
shanshanli <shaniavina@gmail.com>
2017-06-01 23:22:29 -0400

    Update implement_stack_using_queues.py

--- implement_stack_using_queues.py
+++ implement_stack_using_queues.py
@@ -1,33 +1,47 @@
-class Stack(object):
+class MyStack(object):
+
     def __init__(self):
         """
-        initialize your data structure here.
+        Initialize your data structure here.
         """
-        self.A= []
+        self.A = collections.deque()
 
     def push(self, x):
         """
+        Push element x onto stack.
         :type x: int
-        :rtype: nothing
+        :rtype: void
         """
         self.A.append(x)
+        for i in range(len(self.A) - 1):
+            self.A.append(self.A.popleft())
+            
 
     def pop(self):
         """
-        :rtype: nothing
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
         """
-        return self.A.pop()
+        return self.A.popleft()
 
     def top(self):
         """
+        Get the top element.
         :rtype: int
         """
-        return self.A[-1]
+        return self.A[0]
 
     def empty(self):
         """
+        Returns whether the stack is empty.
         :rtype: bool
         """
         return len(self.A) == 0
 
-    
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
