d8b92e9bc551844827ab6d14e6ef1728e0fd2a63
shanshanli <shaniavina@gmail.com>
2017-06-02 17:57:33 -0400

    Update binary_tree_level_order_travesal.py

--- binary_tree_level_order_travesal.py
+++ binary_tree_level_order_travesal.py
@@ -1,9 +1,4 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# iteratively: BFS
 
 class Solution(object):
     def levelOrder(self, root):
@@ -12,7 +7,7 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         if root is None:
-            return []
+            return []   ####base case
         result, current = [], [root]
         while current:
             next_level, vals = [], []
@@ -25,5 +20,38 @@ class Solution(object):
             current = next_level
             result.append(vals)
         return result
+    
+    ####recursively
+    # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+    
+        res = []
+        self.levelOrderRecu(root, res, 0)
+        return res
+        
+    def levelOrderRecu(self, root, res, level):
+        if not root:
+            return None
+        
+        if len(res) == level:  ####level not contained in list
+            res.append([root.val])
+        else:
+            res[level].append(root.val)
+        
+        self.levelOrderRecu(root.left, res, level + 1)
+        self.levelOrderRecu(root.right, res, level + 1)
+        
+        
 
     
