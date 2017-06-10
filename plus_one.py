3a1b2628e11873627709a2273f1d33c53a5de467
shanshanli <shaniavina@gmail.com>
2017-06-09 23:05:13 -0400

    Update plus_one.py

--- plus_one.py
+++ plus_one.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        carry = 1
-        for i in reversed(range(len(digits))):
-            digits[i] += carry
-            carry = digits[i] / 10
-            digits[i] %= 10
+        carry = 1   
+        for i in reversed(range(len(digits))):  #####reversed(range())
+            carry += digits[i]
+            digits[i] = carry % 10
+            carry /= 10
         if carry:
-            digits = [1] + digits
+            digits = [1] + digits  ####list add forward
         return digits
-
-    
+            
+            
+            
+            
