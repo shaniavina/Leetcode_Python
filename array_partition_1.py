6cd1e7ba272da7286db42bf658b324fcf6c783a1
shanshanli <shaniavina@gmail.com>
2017-06-28 21:56:43 -0400

    Update array_partition_1.py

--- array_partition_1.py
+++ array_partition_1.py
@@ -5,3 +5,16 @@ class Solution(object):
         :rtype: int
         """
         return sum(sorted(nums)[::2])
+
+    
+    class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        sorted_nums = sorted(nums)
+        res = 0
+        for i in range(0, len(nums), 2):
+            res += sorted_nums[i]
+        return res
