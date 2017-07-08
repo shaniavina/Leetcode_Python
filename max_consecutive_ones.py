2849720d793d390ae2c9f145e44b2bee7d972967
shanshanli <shaniavina@gmail.com>
2017-07-08 13:13:33 -0400

    Update max_consecutive_ones.py

--- max_consecutive_ones.py
+++ max_consecutive_ones.py
@@ -15,3 +15,20 @@ class Solution(object):
                 count = 0
         return max
                     
+####second 
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        max_count = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
+                max_count = max(count, max_count)
+            else: 
+                count = 0
+        return max_count
+                
