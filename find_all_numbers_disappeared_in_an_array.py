39b7b01678be67394b3a73591b585557f215fbb7
shanshanli <shaniavina@gmail.com>
2017-07-08 14:48:31 -0400

    Update find_all_numbers_disappeared_in_an_array.py

--- find_all_numbers_disappeared_in_an_array.py
+++ find_all_numbers_disappeared_in_an_array.py
@@ -6,8 +6,7 @@ class Solution(object):
         """
         return list(set(range(1, len(nums) + 1)) - set(nums))
         
-    
-    
+   
     ##second solution
     def findDisappearedNumbers2(self, nums):
         for i in range(len(nums)):
