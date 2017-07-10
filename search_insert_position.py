ac4122ea585fc254559d34db4b43076e1d59fdaa
shanshanli <shaniavina@gmail.com>
2017-07-10 00:06:55 -0400

    Update search_insert_position.py

--- search_insert_position.py
+++ search_insert_position.py
@@ -12,3 +12,17 @@ class Solution(object):
                 i += 1
         if target > nums[-1]:
             return len(nums)
+####second
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if target <= nums[i]:
+                return i
+            else:
+                continue
+        return len(nums)
