d167b7dcd1fb43d2b4e84525dc6e9b9f7e0d2364
shanshanli <shaniavina@gmail.com>
2017-07-09 22:47:23 -0400

    Update maximum_products_of_three_numbers.py

--- maximum_products_of_three_numbers.py
+++ maximum_products_of_three_numbers.py
@@ -10,3 +10,26 @@ class Solution(object):
         else:
             return max(sorted_nums[0] * sorted_nums[1] * sorted_nums[-1], sorted_nums[-1] * sorted_nums[-2] * sorted_nums[-3])
                 
+
+            ###without sorted
+        min1, min2 = float('inf'), float('inf')
+        max1, max2, max3 = float('-inf'), float('-inf'), float('-inf')
+        
+        for n in nums:
+            if n < min1:
+                min2 = min1
+                min1 = n
+            elif n < min2:
+                min2 = n
+            
+            if n > max1:
+                max3 = max2
+                max2 = max1
+                max1 = n
+            elif n > max2:
+                max3 = max2
+                max2 = n
+            elif n > max3:
+                max3 = n
+        return max(min1 * min2 * max1, max1 * max2 * max3)
+                
