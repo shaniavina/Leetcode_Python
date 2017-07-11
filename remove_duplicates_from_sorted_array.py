bc09ba15111079ad0c59296b7b71cfe5cc25a406
shanshanli <shaniavina@gmail.com>
2017-07-11 14:13:05 -0400

    Update remove_duplicates_from_sorted_array.py

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -6,12 +6,10 @@ class Solution(object):
         """
         if not nums:
             return 0
-        i, last = 1, 0
+        i, last = 1, 0     ####two pointers
         while i < len(nums):
             if nums[last] != nums[i]:
                 last += 1
                 nums[last] = nums[i]
             i += 1
         return last + 1
-
-    
