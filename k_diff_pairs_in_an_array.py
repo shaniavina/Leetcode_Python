e2255c7a9a8479f59557e7753ba821613c708c6c
shanshanli <shaniavina@gmail.com>
2017-07-12 00:20:32 -0400

    Update k_diff_pairs_in_an_array.py

--- k_diff_pairs_in_an_array.py
+++ k_diff_pairs_in_an_array.py
@@ -5,9 +5,11 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        
+       ####set usage
         if k > 0:
             return len(set(nums) & set(n + k for n in nums))
         elif k == 0:
             return sum(v > 1 for v in collections.Counter(nums).values())
-        else:
+        else:    ####discussion on k < 0
             return 0
