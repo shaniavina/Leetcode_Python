8474b17016d33364dea21da9539ed104359d64ca
shanshanli <shaniavina@gmail.com>
2017-07-12 00:34:28 -0400

    Update third_maximum_number.py

--- third_maximum_number.py
+++ third_maximum_number.py
@@ -5,19 +5,22 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        top = [float('-inf')] * 3
-        
-        for num in nums:
-            if num > top[0]:
-                top[0], top[1], top[2] = num, top[0], top[1]
+        max1, max2, max3 = float('-inf'), float('-inf'), float('-inf')
+        for i in range(len(nums)):
+            if nums[i] > max1:
+                max3 = max2
+                max2 = max1
+                max1 = nums[i]
                 count += 1
-            elif num != top[0] and num > top[1]:     ######have to be distinct
-                top[1], top[2] = num, top[1]
+            elif nums[i] != max1 and nums[i] > max2:
+                max3 = max2
+                max2 = nums[i]
                 count += 1
-            elif num != top[0] and num != top[1] and num > top[2]:
-                top[2] = num
+            elif nums[i] != max2 and nums[i] != max1 and nums[i] > max3:   ####have to be distinct
+                max3 = nums[i]
                 count += 1
         if count < 3:
-            return top[0]
+            return max1
         else:
-            return top[2]
+            return max3
+        
