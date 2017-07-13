3dba075e9dfdc7044331d06946468d76e1d84862
shanshanli <shaniavina@gmail.com>
2017-07-12 23:40:11 -0400

    Update single_number.py

--- single_number.py
+++ single_number.py
@@ -4,7 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # lookup = {}
+        # for num in nums:
+        #     if num in lookup:
+        #         lookup[num] += 1
+        #     else:
+        #         lookup[num] = 0
+        # for key in lookup:
+        #     if lookup[key] == 0:
+        #         return key
+        
+        
+        # ####using set
+        # lookup = set()
+        # for num in nums:
+        #     if num in lookup:
+        #         lookup.remove(num)
+        #     else:
+        #         lookup.add(num)
+        # return lookup.pop()
+        
+        
         res = 0
-        for i in range(len(nums)):
-            res ^= nums[i]
+        for num in nums:
+            res ^= num
         return res
+                
