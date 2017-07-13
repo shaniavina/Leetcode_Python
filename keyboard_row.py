7589d2d60dbeeb95221c63f665102fe8b7cfbf74
shanshanli <shaniavina@gmail.com>
2017-07-12 21:21:54 -0400

    Update keyboard_row.py

--- keyboard_row.py
+++ keyboard_row.py
@@ -4,13 +4,29 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        line1, line2, line3 = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
+        # line1, line2, line3 = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
+        # res = []
+        # for word in words:
+        #     w = set(word.lower())    ###w has to be a set()
+        #     if w.issubset(line1) or w.issubset(line2) or w.issubset(line3):
+        #         res.append(word)
+        # return res
+        
+        
+        ref = [set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')]
         res = []
+        k = 0
         for word in words:
-            w = set(word.lower())
-            if w.issubset(line1) or w.issubset(line2) or w.issubset(line3):
+            
+            for i in range(len(ref)):
+                if word[0].lower() in ref[i]:
+                    k = i
+                    break
+            for w in word:
+                if w.lower() not in ref[k]:
+                    break
+            else:     ########for else loop!!
                 res.append(word)
         return res
-        
-         #second solution
-        return [word for word in words if any(set(word.lower()) <= set(line) for line in ('qwertyuiop', 'asdfghjkl', 'zxcvbnm'))]
+                
+                    
