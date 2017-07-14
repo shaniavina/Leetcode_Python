29f3247f21d4c79fde0275ab5a250c1821da4aae
shanshanli <shaniavina@gmail.com>
2017-07-13 22:22:39 -0400

    Update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -5,26 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
+        if len(s) != len(t):  ####have to compare the length first
             return False
         
-        count = {}
-        
-        for c in s:
-            if c.lower() in count:                     ###.lower() not necessary
-                count[c.lower()] += 1
+        lookup = {}
+        for w in s:
+            if w in lookup:
+                lookup[w] += 1
             else:
-                count[c.lower()] = 1
+                lookup[w] = 1
         
         for c in t:
-            if c.lower() in count:
-                count[c.lower()] -= 1
-            else: 
-                count[c.lower()] = -1
-            if count[c.lower()] < 0:
+            if c not in lookup or lookup[c] == 0:
                 return False
-                
+            else:
+                lookup[c] -= 1
         return True
-
-                
-###.lower() not necessary
+            
