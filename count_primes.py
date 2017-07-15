384ed30cbf8b75810c12cc74c9c654405aae2a57
shanshanli <shaniavina@gmail.com>
2017-07-15 19:29:07 -0400

    Update count_primes.py

--- count_primes.py
+++ count_primes.py
@@ -4,12 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n < 3:
+         #####second
+        if n <= 2:
             return 0
         primes = [True] * n
         primes[0] = primes[1] = False
-        for i in range(2, int(n ** 0.5) + 1):   ####get the integer
-            primes[i * i:n:i] = [False] * len(primes[i * i:n:i])
+        for i in range(2, int(n ** 0.5) + 1):    ####integer
+            if primes[i]:
+                for j in range(i * i, n, i):
+                    primes[j] = False
         return sum(primes)
 
     #####second
