31a2b7c46f95bc4bc06440d9b56c65bfcda49c98
shanshanli <shaniavina@gmail.com>
2017-07-16 01:36:19 -0400

    Update word_pattern.py

--- word_pattern.py
+++ word_pattern.py
@@ -16,3 +16,29 @@ class Solution(object):
             pattern_map[pattern[i]] = i
             words_map[words[i]] = i
         return True
+
+    ######second
+   
+        words = str.split()
+        if len(words) != len(pattern):
+            return False
+        
+        map1 = {}
+        map2 = {}     ####contradict: "abba""dog dog dog dog"
+        for i in range(len(pattern)):
+            if pattern[i] in map1:
+                if map1[pattern[i]] != words[i]:
+                    return False
+            else:
+                map1[pattern[i]] = words[i]
+            if words[i] in map2:
+                if map2[words[i]] != pattern[i]:
+                    return False
+            else:
+                map2[words[i]] = pattern[i]    
+                
+                
+                
+        return True
+            
+        
