e4434ac5d7798baa71715579fe8f04fec3c80cae
shanshanli <shaniavina@gmail.com>
2017-07-15 23:14:27 -0400

    Update number_of_boomerangs.py

--- number_of_boomerangs.py
+++ number_of_boomerangs.py
@@ -4,26 +4,19 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: int
         """
-        ret = 0
-        pos_map = [{} for i in range(len(points))]
+        res = 0
         for i in range(len(points)):
-            for j in range(i + 1, len(points)):
+            count = {}
+            for j in range(len(points)):
                 if j == i:
                     continue
-                distance_square = (points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2
-                if distance_square not in pos_map[i]:
-                    pos_map[i][distance_square] = 1
+                dis = (points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2
+                if dis in count:
+                    count[dis] += 1
+                    res += 2 * (count[dis] - 1)
                 else:
-                    pos_map[i][distance_square] += 1
-                if distance_square not in pos_map[j]:
-                    pos_map[j][distance_square] = 1
-                else:
-                    pos_map[j][distance_square] += 1   
-        for i in range(len(points)):
-            for dis in pos_map[i]:
-                if pos_map[i][dis] > 1:
-                    ret += pos_map[i][dis] * (pos_map[i][dis] - 1)
-        return ret
+                    count[dis] = 1
+        return res
         
         
         
