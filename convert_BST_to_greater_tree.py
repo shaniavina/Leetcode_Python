93f54210b84256de7fc4d03cc6e008354cdee1f8
shanshanli <shaniavina@gmail.com>
2017-07-19 00:03:10 -0400

    Update convert_BST_to_greater_tree.py

--- convert_BST_to_greater_tree.py
+++ convert_BST_to_greater_tree.py
@@ -6,27 +6,35 @@
 #         self.right = None
 
 class Solution(object):
+    # def __init__(self):
+    #     self.sum = 0      #######sum == 0 for the beginning
+    # def convertBST(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: TreeNode
+    #     """
+    #     ####in order traversal(recursion, iteration[stack]), this one doesn't need helper funciton, better!!
+    #     if not root:
+    #         return None
+    #     self.convertBST(root.right)
+    #     root.val += self.sum
+    #     self.sum = root.val
+    #     self.convertBST(root.left)
+    #     return root
+    
+    
+    #####recursion, helper, reverse inorder traversal
     def convertBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-        def visit1(root):
-            if root:
-                visit1(root.left)
-                vals.append(root.val)
-                visit1(root.right)
-        vals = []
-        visit1(root)
-        
 
-        self.s = 0        #####weird
-        def visit2(root):
-            if root:
-                visit2(root.right)
-                self.s += vals.pop()
-                root.val = self.s
-                visit2(root.left)
-        visit2(root)
-        
+        self.convertBSTHelper(root, 0)
         return root
+    def convertBSTHelper(self, root, sum):
+        if not root:
+            return sum
+        if root.right:
+            sum = self.convertBSTHelper(root.right, sum)
+        root.val += sum
+        sum = root.val
+        if root.left:
+            sum = self.convertBSTHelper(root.left, sum)
+        return sum
