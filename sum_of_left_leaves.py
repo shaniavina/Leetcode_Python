c04066b74f895586fb171b9f3ffdb879373179af
shanshanli <shaniavina@gmail.com>
2017-07-22 17:32:20 -0400

    Update sum_of_left_leaves.py

--- sum_of_left_leaves.py
+++ sum_of_left_leaves.py
@@ -11,8 +11,24 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        # ####recursion
+        # if not root:
+        #     return 0
+        # if root.left and not root.left.left and not root.left.right:
+        #     return root.left.val + self.sumOfLeftLeaves(root.right)
+        # return self.sumOfLeftLeaves(root.right) + self.sumOfLeftLeaves(root.left)
+        
+        ####iteration, pre-order traversal, stack
         if not root:
             return 0
-        if root.left and not root.left.left and not root.left.right:
-            return root.left.val + self.sumOfLeftLeaves(root.right)
-        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+        res = 0
+        nodes = [root]
+        while nodes:
+            node = nodes.pop()
+            if node.left and not node.left.left and not node.left.right:
+                res += node.left.val
+            if node.left:
+                nodes.append(node.left)
+            if node.right:
+                nodes.append(node.right)
+        return res
