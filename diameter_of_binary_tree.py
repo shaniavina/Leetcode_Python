f47eef248deb455e32554d286827dd3a10d5f9a4
shanshanli <shaniavina@gmail.com>
2017-07-23 22:18:16 -0400

    Update diameter_of_binary_tree.py

--- diameter_of_binary_tree.py
+++ diameter_of_binary_tree.py
@@ -11,13 +11,19 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        self.best = 1
-        def depth(root):
-            if not root:
-                return 0
-            ansL = depth(root.left)
-            ansR = depth(root.right)
-            self.best = max(self.best, ansL + ansR + 1)
-            return 1 + max(ansL, ansR)
-        depth(root)
-        return self.best - 1
+        self.best = 0
+        self.map = {}
+        self.depth(root)
+        return self.best
+    
+    def depth(self, root):
+        if not root:
+            return 0
+        if root in self.map:      ######optimization, using hash table
+            return self.map[root]
+        
+        left = self.depth(root.left)
+        right = self.depth(root.right)
+        self.best = max(self.best, left + right)
+        self.map[root] = 1 + max(left, right)
+        return self.map[root]
