fe503c8e7bb5d43495a2a49052e49926bc4b4b61
shanshanli <shaniavina@gmail.com>
2017-07-23 21:38:59 -0400

    Update same_tree.py

--- same_tree.py
+++ same_tree.py
@@ -12,13 +12,10 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        if p is None and q is None:
+        if not p and not q:
             return True
-        if p is not None and q is not None:
+        if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-    
         return False
-if __name__ == "__main__":
-    root1, root1.left, root1.right = TreeNode(1), TreeNode(2), TreeNode(3)
-    root2, root2.left, root2.right = TreeNode(1), TreeNode(2), TreeNode(3)
-    print Solution().isSameTree(root1, root2)
+    
+    #####also can use traversal
