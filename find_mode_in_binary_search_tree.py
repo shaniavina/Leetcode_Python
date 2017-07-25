2a2952c260abe72c90a561765e17394666af0e1a
shanshanli <shaniavina@gmail.com>
2017-07-24 22:18:58 -0400

    Update find_mode_in_binary_search_tree.py

--- find_mode_in_binary_search_tree.py
+++ find_mode_in_binary_search_tree.py
@@ -6,28 +6,56 @@
 #         self.right = None
 
 class Solution(object):
-    def helper(self, root, count):
-        
-        if root is None:
-            return None
-        if root.val in count:
-            count[root.val] += 1
-        else:
-            count[root.val] = 1
-        self.helper(root.left, count)
-        self.helper(root.right, count)
-        return 
-    
     def findMode(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
         
-        if root is None:
-            return []
-        count = {}
-        self.helper(root, count)
-        max_freq = max(count.itervalues())
-        res = [k for k, v in count.iteritems() if v == max_freq]
-        return res
+        #####using BST feature, inorder traversal
+        #######doesn't work through
+        pre, count, maxi, res = None, 0, 1, []
+        classi = [pre, count, maxi, res]
+        self.inorder(root, classi)
+        return classi[3]
+    
+    def inorder(self, node, classi):
+        if not node:
+            return None
+        self.inorder(node.left, classi)
+        if classi[0]:
+            if classi[0].val == node.val:
+                classi[1] += 1
+            else:
+                classi[1] = 1
+        if classi[1] >= classi[2]:
+            if classi[1] > classi[2]:
+                classi[3] = []
+            classi[3].append(node.val)
+            classi[2] = classi[1]
+        classi[0] = node
+        self.inorder(node.right, classi)
+        
+            
+            
+#         ####traverse, use hash table(extra space)
+#         if not root:
+#             return []
+#         count = {}
+#         self.findModeRecu(root, count)
+#         max_freq = max(count.itervalues())
+#         res = [k for k, v in count.iteritems() if v == max_freq]
+#         return res
+#     def findModeRecu(self, root, count):
+#         if not root:
+#             return None
+#         if root.val in count:
+#             count[root.val] += 1
+#         else:
+#             count[root.val] = 1
+        
+#         self.findModeRecu(root.left, count)
+#         self.findModeRecu(root.right, count)
+#         return
+        
+    
