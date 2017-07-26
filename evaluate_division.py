577ff258dfffd02042899f0e61ed2e330907422e
shanshanli <shaniavina@gmail.com>
2017-07-25 23:10:32 -0400

    Update evaluate_division.py

--- evaluate_division.py
+++ evaluate_division.py
@@ -6,26 +6,28 @@ class Solution(object):
         :type queries: List[List[str]]
         :rtype: List[float]
         """
-        def check(up, down, lookup, visited):
-            if up in lookup and down in lookup[up]:
-                return (True, lookup[up][down])
-            for k, v in lookup[up].iteritems():
-                if k not in visited:
-                    visited.add(k)
-                    tmp = check(k, down, lookup, visited)
-                    if tmp[0]:
-                        return (True, v * tmp[1])
-            return (False, 0)
-            
         lookup = collections.defaultdict(dict)
         for i, e in enumerate(equations):
             lookup[e[0]][e[1]] = values[i]
             if values[i]:
                 lookup[e[1]][e[0]] = 1.0 / values[i]
-
-        result = []
+        res = []
         for q in queries:
             visited = set()
-            tmp = check(q[0], q[1], lookup, visited)
-            result.append(tmp[1] if tmp[0] else -1)
-        return result
+            tmp = self.check(q[0], q[1], lookup, visited)
+            res.append(tmp[1] if tmp[0] else -1.0)
+        return res
+    
+    def check(self, up, down, lookup, visited):
+        
+        if up in lookup and down in lookup[up]:
+            return [True, lookup[up][down]]
+        for k, v in lookup[up].iteritems():
+            if k not in visited:
+                visited.add(k)
+                tmp = self.check(k, down, lookup, visited)
+                if tmp[0]:
+                    return [True, v * tmp[1]]
+        return [False, 0]
+    
+    #####second, using graph, BFS, queue, to be completed
