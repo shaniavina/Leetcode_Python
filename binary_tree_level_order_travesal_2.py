e0293d2c2e35253f99a78a97bf9b17669dd9493d
shanshanli <shaniavina@gmail.com>
2017-07-26 21:27:40 -0400

    Update and rename binary_tree_level_order_travesal2.py to binary_tree_level_order_travesal_2.py

---
+++ binary_tree_level_order_travesal_2.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+####level order, BFS, queue
+####reverse the result of 102
+        if not root:
+            return []
+        q = [root]
+        res = []
+        
+        while q:
+            next_q = collections.deque([])
+            tmp_res = []
+            for node in q:
+                tmp_res.append(node.val)
+                if node.left:
+                    next_q.append(node.left)
+                if node.right:
+                    next_q.append(node.right)
+            res.insert(0, tmp_res)       #####instead of .reverse(), you can use insert on the top of queue
+            q = next_q
+        
+        return res
+        
