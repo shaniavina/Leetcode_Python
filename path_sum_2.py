4769cd9f2fe844000a1dc4d0f9848417697a7ffa
shanshanli <shaniavina@gmail.com>
2017-07-28 00:57:13 -0400

    Update path_sum_2.py

--- path_sum_2.py
+++ path_sum_2.py
@@ -25,3 +25,38 @@ class Solution(object):
         self.pathSumRecu(result, cur, root.right, sum - root.val)
         cur.pop()
         return result
+
+    
+    ####almost the same
+    # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        
+        res = []
+        self.pathSumRecu(root, sum, [], res)
+        return res
+        
+    def pathSumRecu(self, node, sum, path, res):
+        if not node:
+            return
+        path.append(node.val)
+        if not node.left and not node.right and node.val == sum:
+            res.append(path + [])   ###append a list is a shallow copy; append a string or number is a deep copy
+        
+        if node.left:
+            self.pathSumRecu(node.left, sum - node.val, path, res)
+        if node.right:
+            self.pathSumRecu(node.right, sum - node.val, path, res)
+        path.pop()
+            
