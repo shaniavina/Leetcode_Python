dfc4418e8bf8a07d6268a489192b1c032895e63b
shanshanli <shaniavina@gmail.com>
2017-07-27 21:46:55 -0400

    Update convert_sorted_list_to_binary_search_tree.py

--- convert_sorted_list_to_binary_search_tree.py
+++ convert_sorted_list_to_binary_search_tree.py
@@ -20,15 +20,39 @@ class Solution(object):
         cur, length = head, 0
         while cur:
             cur, length = cur.next, length + 1
-        self.head = head
-        return self.sortedListToBSTRecu(0, length)
-    def sortedListToBSTRecu(self, start, end):
-        if start == end:
+        return self.sortedListToBSTRecu(head, length)
+    def sortedListToBSTRecu(self, head, length):
+        if length == 0:
             return None
-        mid = (start + end) / 2
-        left = self.sortedListToBSTRecu(start, mid)
-        cur = TreeNode(self.head.val)
-        cur.left = left
-        self.head = self.head.next
-        cur.right = self.sortedListToBSTRecu(mid + 1, end)
+        guard = ListNode(0)
+        guard.next = head
+        slow, quick = guard, head
+        
+        mid = length / 2
+        for i in range(mid):
+            slow = slow.next   ####step diff is 1; since there is one middle point
+            quick = quick.next
+        slow.next = None
+        cur = TreeNode(quick.val)
+        cur.left = self.sortedListToBSTRecu(head, mid)
+        cur.right = self.sortedListToBSTRecu(quick.next, length - mid - 1)
         return cur
+        
+        
+#         cur, length = head, 0
+#         while cur:
+#             cur, length = cur.next, length + 1
+        
+#         return self.sortedListToBSTRecu(0, length, head)[0]
+#     def sortedListToBSTRecu(self, start, end, pointer):
+#         if start == end:
+#             return None, pointer
+#         mid = (start + end) / 2
+#         left, pointer = self.sortedListToBSTRecu(start, mid, pointer)
+#         cur = TreeNode(pointer.val)
+#         cur.left = left
+#         pointer = pointer.next
+#         cur.right, pointer = self.sortedListToBSTRecu(mid + 1, end, pointer)
+#         return cur, pointer
+    
+    
