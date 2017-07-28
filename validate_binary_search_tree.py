825030724240e8d290fb7ca2086722ce3b281b60
shanshanli <shaniavina@gmail.com>
2017-07-27 23:00:16 -0400

    Update validate_binary_search_tree.py

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -1,5 +1,9 @@
-# in order traversal
-####doesn't work if you have duplicate values
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
     def isValidBST(self, root):
@@ -7,37 +11,58 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        pre = float('-inf')
-        return self.isValidBSTRecu(root, pre)
-    def isValidBSTRecu(self, root, pre):
-        if not root:
+        pre = [None]
+        return self.isValidBSTRecu(root, pre)     ###inorder traversal; pre is global variable; or you can return 'pre' in function
+    def isValidBSTRecu(self, node, pre):
+        if not node:
             return True
-        if not self.isValidBSTRecu(root.left, pre):
+        if not self.isValidBSTRecu(node.left, pre):
             return False
-        if not pre and root.val <= pre:
+        if pre[0] and node.val <= pre[0].val:
             return False
-        pre = root.val
-        if not self.isValidBSTRecu(root.right, pre):
+        pre[0] = node
+        if not self.isValidBSTRecu(node.right, pre):
             return False
         return True
-  
-  
-  
-  ######min and max
-  
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
         
-        return self.isValidBSTRecu(root, float("-inf"), float("inf"))
-        
-    def isValidBSTRecu(self, root, low, high):
-        if not root:
-            return True
-        return low < root.val and root.val < high \
-            and self.isValidBSTRecu(root.left, low, root.val) \
-            and self.isValidBSTRecu(root.right, root.val, high)
+        #####amazing
+    #     return self.isValidBSTRecu(root, float('-inf'), float('inf'))
+    # def isValidBSTRecu(self, node, low, high):
+    #     if not node:
+    #         return True
+    #     return low < node.val and node.val < high\
+    #         and self.isValidBSTRecu(node.left, low, node.val) and self.isValidBSTRecu(node.right, node.val, high)
+    
+    
+#     #####using stack 
+#     class Solution:
+#     def traverseBST(self, root):
+#         if root == None:
+#             return True
+#         else:
+#             n = root
+#             stack = []
+#             while n != None:
+#                 stack.append(n)
+#                 n = n.left
             
+#             pre = None
+#             while len(stack):
+#                 n = stack.pop()
+#                 if pre != None:
+#                     if pre >= n.val:
+#                         return False
+#                 pre = n.val
+                
+#                 if n.right != None:
+#                     nr = n.right
+#                     while nr != None:
+#                         stack.append(nr)
+#                         nr = nr.left
+#             return True
+#     # @param root, a tree node
+#     # @return a boolean
+#     def isValidBST(self, root):
+#         return self.traverseBST(root)
+    
+    
