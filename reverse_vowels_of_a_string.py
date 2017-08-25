f495a516d4874d33b213b248460af65988287b5e
shanshanli <shaniavina@gmail.com>
2017-08-25 14:24:56 -0400

    Update reverse_vowels_of_a_string.py

--- reverse_vowels_of_a_string.py
+++ reverse_vowels_of_a_string.py
@@ -4,23 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        vowel_set = set(['a', 'e', 'i', 'o', 'u'])
+        s = list(s)
         vowels = []
-        vowel_pos = []
-        str_list = []
-        
+        for c in s:
+            if c in ['a','e','i','o','u','A','E','I','O','U']:###upper, lower case
+                vowels.append(c)
         for i in range(len(s)):
-            str_list.append(s[i])
-            if s[i].lower() in vowel_set:
-                vowels.append(s[i])
-                vowel_pos.append(i)
-                
-        vowels.reverse()
-        
-        for i in range(len(vowels)):
-            str_list[vowel_pos[i]] = vowels[i]
-            
-        return "".join(str_list)
-        
-        
-        
+            if s[i] in ['a','e','i','o','u','A','E','I','O','U']:
+                s[i] = vowels.pop()
+        return ''.join(s)
