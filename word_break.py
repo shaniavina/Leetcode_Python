d480e38d5f1aab6962e7c5485737efe4c96e2d15
shanshanli <shaniavina@gmail.com>
2017-08-31 00:37:18 -0400

    Create word_break.py

---
+++ word_break.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        
+        ##d[i] is True if there is a word in the dictionary that ends at ith index of s AND d is also True at the beginning of the word
+        d = [False] * len(s)
+        for i in range(len(s)):
+            for w in wordDict:
+                if w == s[i - len(w) + 1: i + 1] and (i - len(w) == -1 or d[i - len(w)]):
+                    d[i] = True
+        return d[-1]
