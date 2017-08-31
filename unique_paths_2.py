bdd218544834f3f63a94e57cbef35e4397663026
shanshanli <shaniavina@gmail.com>
2017-08-31 01:14:34 -0400

    Update unique_paths_2.py

--- unique_paths_2.py
+++ unique_paths_2.py
@@ -4,26 +4,31 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        ways = [0] * n
+        class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        obstacleGrid[0][0] = 1 - obstacleGrid[0][0]
         
-        if obstacleGrid[0][0] == 0:
-            ways[0] = 1
-            
-        for j in xrange(1, n):
-            if obstacleGrid[0][j] == 1:
-                ways[j] = 0
+        for i in range(1, n):
+            if not obstacleGrid[0][i]:
+                obstacleGrid[0][i] = obstacleGrid[0][i - 1]
             else:
-                ways[j] = ways[j - 1]
+                obstacleGrid[0][i] = 0
+        for i in range(1, m):
+            if not obstacleGrid[i][0]:
+                obstacleGrid[i][0] = obstacleGrid[i - 1][0]
+            else:
+                obstacleGrid[i][0] = 0
         
-        for i in xrange(1, m):
-            if obstacleGrid[i][0] == 1:
-                ways[0] = 0
-                
-            for j in xrange(1, n):
-                if obstacleGrid[i][j] == 1:
-                    ways[j] = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if not obstacleGrid[i][j]:
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1]+obstacleGrid[i-1][j]
                 else:
-                    ways[j] += ways[j - 1]
-        
-        return ways[n - 1]
+                    obstacleGrid[i][j] = 0
+        return obstacleGrid[-1][-1]
