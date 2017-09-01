35f5b47abd480177e60a41c389111a3da406d8c7
shanshanli <shaniavina@gmail.com>
2017-09-01 08:47:51 -0400

    Update longest_palindromic_substring.py

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -20,21 +20,51 @@ class Solution(object):
         
         #######second
         
-        class Solution(object):
+     class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        if not s:
-            return 0
-        maxLen = 1
-        start = 0
-        for i in range(len(s)):
-            if i - maxLen >= 1 and s[i - maxLen - 1:i + 1] == s[i - maxLen - 1:i + 1][::-1]:
-                start = i - maxLen - 1
-                maxLen += 2
-            if i - maxLen >= 0 and s[i - maxLen:i + 1] == s[i - maxLen:i + 1][::-1]:
-                start = i - maxLen
-                maxLen += 1
-        return s[start: start + maxLen]
+        # ###when you increase s by 1 character, you could only increase maxPalindromeLen by 1 or 2
+        # if not s:
+        #     return None
+        # maxLen = 1
+        # start = 0
+        # for i in range(1, len(s)):
+        #     if i - maxLen >= 1 and s[i - maxLen - 1 : i + 1] == s[i - maxLen - 1 : i + 1][::-1]:
+        #         start = i - maxLen - 1
+        #         maxLen += 2
+        #         continue
+        #     if i - maxLen >= 0 and s[i - maxLen : i + 1] == s[i - maxLen : i + 1][::-1]:
+        #         start = i - maxLen
+        #         maxLen += 1
+        # return s[start : start + maxLen]
+        
+        
+        
+        
+        #Manacher algorithm in Python O(n)
+       # Transform S into T.
+        # For example, S = "abba", T = "^#a#b#b#a#$".
+        # ^ and $ signs are sentinels appended to each end to avoid bounds checking
+        t = '#'.join('^{}$'.format(s))
+        n = len(t)
+        p = [0] * n
+        center = radius = 0
+        for i in range (1, n - 1):
+            if radius > i:
+                p[i] = min(radius - i, p[2*center - i]) 
+            # Attempt to expand palindrome centered at i
+            while t[i + 1 + p[i]] == t[i - 1 - p[i]]:
+                p[i] += 1
+    
+            # If palindrome centered at i expand past R,
+            # adjust center based on expanded palindrome.
+            if i + p[i] > radius:
+                center, radius = i, i + p[i]
+    
+        # Find the maximum element in P.
+        maxLen, centerIndex = max((n, i) for i, n in enumerate(p))
+        return s[(centerIndex  - maxLen)/2: (centerIndex  + maxLen)/2]
+    
