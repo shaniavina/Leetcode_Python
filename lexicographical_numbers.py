afce8a141e69645d178be3a82b97c0c0091db85b
shanshanli <shaniavina@gmail.com>
2017-09-03 00:42:02 -0400

    Update lexicographical_numbers.py

--- lexicographical_numbers.py
+++ lexicographical_numbers.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        highdigit = 1
-        while highdigit * 10 <= n:
-            highdigit *= 10
-        higherdigit = highdigit * 10
+        high = 1
+        while high * 10 <= n:
+            high *= 10
+        higher = high * 10
         withkeys = []
         for i in range(1, n + 1):
             key = i
-            while key < highdigit:
+            while key < high:
                 key *= 10
-            withkeys.append(key * higherdigit + i)
+            withkeys.append(key * higher + i)
         withkeys.sort()
-        return [ki % higherdigit for ki in withkeys]
+        return [ki % higher for ki in withkeys]
