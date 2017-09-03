73be9d47ef186d5977e1b01e2e548adfbe8203fc
shanshanli <shaniavina@gmail.com>
2017-09-03 10:49:49 -0400

    Update H_index_2.py

--- H_index_2.py
+++ H_index_2.py
@@ -5,11 +5,11 @@ class Solution(object):
         :rtype: int
         """
         n = len(citations)
-        left, right = 0, n - 1
-        while left <= right:
-            mid = (left + right) / 2
-            if citations[mid] >= n -mid:
-                right = mid - 1
+        l, r = 0, n-1
+        while l <= r:
+            mid = l + (r - l)/2
+            if citations[mid] >= n-mid:
+                r = mid - 1
             else:
-                left = mid + 1
-        return n - left
+                l = mid + 1
+        return n-l
