0c5576cd57867b029dfb19907bc92270b478bce2
shanshanli <shaniavina@gmail.com>
2017-09-04 18:20:16 -0400

    Update remove_duplicates_from_sorted_array_2.py

--- remove_duplicates_from_sorted_array_2.py
+++ remove_duplicates_from_sorted_array_2.py
@@ -15,3 +15,18 @@ class Solution(object):
             i += 1
         return last + 1
         
+
+        
+        class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        tail = 0
+        for num in nums:
+            if tail < 2 or num != nums[tail - 1] or num != nums[tail - 2]:
+                nums[tail] = num
+                tail += 1
+        return tail
