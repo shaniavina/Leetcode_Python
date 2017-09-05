ff48497bfbd4a67dd36464bc274ebf07c17e2f2c
shanshanli <shaniavina@gmail.com>
2017-09-04 20:36:45 -0400

    Update flatten_binary_tree_to_linked_list.py

--- flatten_binary_tree_to_linked_list.py
+++ flatten_binary_tree_to_linked_list.py
@@ -11,16 +11,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: void Do not return anything, modify root in-place instead.
         """
-        #####interesting. other recu function
         if not root:
             return 
-        self.flatten(root.right)
-        if not root.left:
-            return 
-        self.flatten(root.left)
-        tail = root.left
-        while tail.right:
-            tail = tail.right
-        tail.right = root.right
-        root.right = root.left
-        root.left = None
+        cur=TreeNode(-1)
+        q=[root]
+        while q:
+            node = q.pop()
+            cur.right = node
+            cur.left = None
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+            cur = node
