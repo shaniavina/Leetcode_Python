648b7c3dfe105acb7a0b0a94bc74d5032dbe6f13
shanshanli <shaniavina@gmail.com>
2017-09-17 12:07:54 -0400

    Update valid_sudoku.py

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -4,38 +4,26 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        ##check rows
-        
-        for i in range(9):
-            nSet = set()
-            for j in range(9):
-                if board[i][j] != '.':
-                    if board[i][j] not in nSet:
-                        nSet.add(board[i][j])
-                    else:
-                        return False
-        ##check cols
-        
-        for i in range(9):
-            nSet = set()
-            for j in range(9):
-                if board[j][i] != '.':
-                    if board[j][i] not in nSet:
-                        nSet.add(board[j][i])
-                    else:
-                        return False
-        ##check sub square
-        for i in range(3):
-            for j in range(3):
-                nSet = set()
-                for m in range(3):
-                    for n in range(3):
-                        x = i * 3 + m
-                        y = j * 3 + n
-                        if board[x][y] != '.':
-                            if board[x][y] not in nSet:
-                                nSet.add(board[x][y])
-                            else:
-                                return False
-        return True
+        ###just make sure there is unique value in each row, each col, each square
+        return self.isValidRow(board) and self.isValidCol(board) and self.isValidSquare(board)
     
+    def isValidRow(self, board):
+        for row in board:
+            if not self.isValidUnit(row):
+                return False
+        return True
+    def isValidCol(self, board):
+        for col in zip(*board):
+            if not self.isValidUnit(col):
+                return False
+        return True
+    def isValidSquare(self, board):
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.isValidUnit(square):
+                    return False
+        return True
+    def isValidUnit(self, unit):
+        unit = [i for i in unit if i != '.']
+        return len(set(unit)) == len(unit)
