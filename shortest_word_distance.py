a363e9aad7f3c7e779fde6edb4630d878e34023c
Shanshan Li @Stony Brook <shaniavina@gmail.com>
2018-03-08 23:44:44 -0500

    Update shortest_word_distance.py

--- shortest_word_distance.py
+++ shortest_word_distance.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def shortestDistance(self, words, word1, word2):
+        ###idx_* assign value as -1
         idx_1, idx_2, res = -1, -1, len(words)
         for i in range(len(words)):
             if words[i] == word1:
