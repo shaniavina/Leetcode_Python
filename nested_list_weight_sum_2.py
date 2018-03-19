6430033618e46db94639ad482246ea86925ba5ea
Shanshan Li @JPM <shaniavina@gmail.com>
2018-03-18 22:57:39 -0400

    Update nested_list_weight_sum_2.py

--- nested_list_weight_sum_2.py
+++ nested_list_weight_sum_2.py
@@ -1,42 +1,37 @@
-from scipy.constants.constants import electron_mass
+
 class Solution(object):
-    def nestedWeightSum(self, nestedList):
+    def nestedWeightSum(self, nestedls):
         weighted, unweighted = 0, 0
-        while nestedList:
+        while nestedls:
             nextLevel = []
-            for ele in nestedList:
-                if not isinstance(ele, (list)):
+            for ele in nestedls:
+                if not isinstance(ele, list):
                     unweighted += ele
                 else:
-                    nextLevel.append(ele)
+                    nextLevel += ele
             weighted += unweighted
-            nestedList = nextLevel
+            nestedls = nextLevel
         return weighted
 slt = Solution()
 print(slt.nestedWeightSum([[1,1],2,[1,1]]))
 print(slt.nestedWeightSum([1,[4,[6]]]))
 
 
-###second
+##second
 class Solution(object):
-    def depthSumInverse(self, nestedList):
+    def depthSumInverse(self, nestedls):
         self.res = []
-        for ele in nestedList:
+        for ele in nestedls:
             self.depthSumInverseHelper(ele, 0)
-        
         sum = 0
         for i in reversed(range(len(self.res))):
             sum += self.res[i] * (len(self.res) - i)
         return sum
-    def depthSumInverseHelper(self, list, depth):
+    def depthSumInverseHelper(self, ls, depth):
         if len(self.res) < depth + 1:
             self.res.append(0)
-        if list.isInteger():
-            self.res[depth] += list.getInteger()
+        if ls.isInteger():
+            self.res[depth] += ls.getInteger()
         else:
-            for l in list.getList():
+            for l in ls.getList():
                 self.depthSumInverseHelper(l, depth + 1)
-            
-        
-
-
