dc1df97cd6217536d789caf5d6fdbcdf867ea51e
Shanshan Li @JPM <shaniavina@gmail.com>
2018-03-22 00:18:35 -0400

    Update binary_tree_level_order_travesal.py

--- binary_tree_level_order_travesal.py
+++ binary_tree_level_order_travesal.py
@@ -11,36 +11,36 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        ####recursion, (root, level, res)
-        res = []
-        self.levelOrderRecu(root, 0, res)
-        return res
-    def levelOrderRecu(self, root, level, res):
-        if not root:
-            return
-        if len(res) == level:
-            res.append([root.val])
-        else:
-            res[level].append(root.val)
-        self.levelOrderRecu(root.left, level + 1, res)
-        self.levelOrderRecu(root.right, level + 1, res)
+        
+    #     ####recursion, (root, level)
+    #     self.res = []
+    #     self.levelOrderRecu(root, 0)
+    #     return self.res
+    # def levelOrderRecu(self, node, level):
+    #     if not node:
+    #         return
+    #     if len(self.res) == level:
+    #         self.res.append([node.val])
+    #     else:
+    #         self.res[level].append(node.val)
+    #     self.levelOrderRecu(node.left, level + 1)
+    #     self.levelOrderRecu(node.right, level + 1)
+      
     
-#         ####level order, BFS, queue
+# #         ####level order, BFS, queue
 #         if not root:
 #             return []
-#         q = [root]
+#         q = collections.deque([root])
 #         res = []
-        
 #         while q:
 #             next_q = collections.deque([])
-#             tmp_res = []
+#             tmp = []
 #             for node in q:
-#                 tmp_res.append(node.val)
+#                 tmp.append(node.val)
 #                 if node.left:
 #                     next_q.append(node.left)
 #                 if node.right:
 #                     next_q.append(node.right)
-#             res.append(tmp_res)
+#             res.append(tmp)
 #             q = next_q
 #         return res
-        
