2cd51788b683a76415619161344b5abaa2323606
Shanshan Li @JPM <shaniavina@gmail.com>
2018-03-23 11:46:50 -0400

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -4,18 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        result = []
-        used = [False] * len(nums)
-        self.permuteHelper(result, used, [], nums)
-        return result
-    def permuteHelper(self, result, used, cur, nums):
-        if len(cur) == len(nums):
-            result.append(cur + [])
-            return
-        for i in range(len(nums)):
-            if not used[i]:
-                used[i] = True
-                cur.append(nums[i])
-                self.permuteHelper(result, used, cur, nums)
-                cur.pop()
-                used[i] = False
+        perms = [[]]
+        for n in nums:
+            new_perms = []
+            for perm in perms:
+                for i in range(len(perm) + 1):
+                    new_perms.append(perm[:i] + [n] + perm[i:])
+            perms = new_perms
+        return perms
