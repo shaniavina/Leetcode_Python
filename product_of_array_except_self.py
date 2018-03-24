1fcbe4a993699896fd50d0194b94a65711d15e82
Shanshan Li @JPM <shaniavina@gmail.com>
2018-03-24 11:38:07 -0400

    Update product_of_array_except_self.py

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        output = []
-        
+        #The idea is simply. The product basically is calculated using the numbers before the current number and the numbers after the current number. Thus, we can scan the array twice. First, we calcuate the running product of the part before the current number. Second, we calculate the running product of the part after the current number through scanning from the end of the array.
         p = 1
-        for i in range(len(nums)):
-            output.append(p)
+        n = len(nums)
+        res = []     ###[0] * n
+        for i in range(n):
+            res.append(p)
             p *= nums[i]
-        
         p = 1
-        for i in range(len(nums) - 1, -1, -1):
-            output[i] *= p
+        for i in reversed(range(n)):
+            res[i] *= p
             p *= nums[i]
-        return output
+        return res
+        
+        
+    
