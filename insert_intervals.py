194aaabff73db8b33a4d0491646bf426b69bade3
Shanshan Li @JPM <shaniavina@gmail.com>
2018-03-26 14:05:48 -0400

    Create insert_intervals.py

---
+++ insert_intervals.py
@@ -0,0 +1,25 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        result = []
+        i = 0
+        while i < len(intervals) and newInterval.start > intervals[i].end:
+            result += intervals[i],
+            i += 1
+        while i < len(intervals) and newInterval.end >= intervals[i].start:
+            newInterval = Interval(min(newInterval.start, intervals[i].start), \
+                                   max(newInterval.end, intervals[i].end))
+            i += 1
+        result += newInterval,
+        result += intervals[i:]
+        return result
