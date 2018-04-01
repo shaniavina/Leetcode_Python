ab6c9a117d1e1f5a5659f30ac9e3a7c7d4573fb2
Shanshan Li @JPM <shaniavina@gmail.com>
2018-04-01 18:00:46 -0400

    Update partition_equal_subset_sum.py

--- partition_equal_subset_sum.py
+++ partition_equal_subset_sum.py
@@ -4,21 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        cache = {}
-        def helper(start, target):         
-            if (start, target) in cache:
-                return cache[(start, target)]
-            if target < 0:
-                return
-            elif target == 0:
-                return True
-            for i in xrange(start, len(nums)):
-                if helper(i+1, target-nums[i]):
-                    return True
-            cache[(start, target)] = False
+        ####same as partition into k groups
+        total = sum(nums)
+        k = 2
+        if total % k or total / k < max(nums):
             return False
-        return False if sum(nums)%2 else helper(0, sum(nums)/2)
         
-        
-        
-        ######many solutions unpassed
+        nums.sort(reverse = True)
+        self.subset_sums = [0] * k
+        return self.dfs(nums, total / k, 0)
+    def dfs(self, nums, target, i):
+        if i == len(nums):
+            return True
+        for k in range(len(self.subset_sums)):
+            if self.subset_sums[k] + nums[i] > target:
+                continue
+            self.subset_sums[k] += nums[i]
+            if self.dfs(nums, target, i + 1):
+                return True
+            self.subset_sums[k] -= nums[i]
+            if not self.subset_sums[k]:
+                return False    
