2923681a592e2522a6204f54ca4165e9847a8c8c
Shanshan Li @JPM <shaniavina@gmail.com>
2018-04-09 23:03:57 -0400

    Update number_of_islands.py

--- number_of_islands.py
+++ number_of_islands.py
@@ -3,30 +3,23 @@ class Solution(object):
         """
         :type grid: List[List[str]]
         :rtype: int
-        
         """
         if not grid:
             return 0
-        row = len(grid)
-        col = len(grid[0])
-        used = [[False for j in range(col)] for i in range(row)]
-        
         count = 0
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == '1' and not used[i][j]:
-                    self.dfs(grid, used, row, col, i, j)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
                     count += 1
         return count
-    def dfs(self, grid, used, row, col, x, y):
-        if grid[x][y] == '0' or used[x][y]:
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
             return
-        used[x][y] = True
-        if x != 0:
-            self.dfs(grid, used, row, col, x - 1, y)
-        if x != row - 1:
-            self.dfs(grid, used, row, col, x + 1, y)
-        if y != 0:
-            self.dfs(grid, used, row, col, x, y - 1)
-        if y != col - 1:
-            self.dfs(grid, used, row, col, x, y + 1)
+        grid[i][j] = '#'
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i, j + 1)
+        
+     
