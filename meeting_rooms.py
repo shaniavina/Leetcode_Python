ea56472030438149fd08757d799588cb8ef02200
Shanshan Li @JPM <shaniavina@gmail.com>
2018-04-11 17:55:49 -0400

    Create meeting_rooms.py

---
+++ meeting_rooms.py
@@ -0,0 +1,18 @@
+# Time:  O(nlogn)
+# Space: O(n)
+#
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    # @param {Interval[]} intervals
+    # @return {boolean}
+    def canAttendMeetings(self, intervals):
+        intervals.sort(key = lambda x: x.start)
+        for i in range(1, len(intervals)):
+            if intervals[i].start < intervals[i - 1].end:
+                return False
+        return True
