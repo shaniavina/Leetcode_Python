850fb99ef7b5759238471c70c7ef1bb7922fc416
Shanshan Li @JPM <shaniavina@gmail.com>
2018-04-16 23:37:19 -0400

    Update nested_list_weight_sum_2.py

--- nested_list_weight_sum_2.py
+++ nested_list_weight_sum_2.py
@@ -1,37 +1,31 @@
+###--------------------inverse weighted sum--------------------###
+def inverseDepthSum(nested_list):
+    weighted, unweighted = 0, 0
+    while nested_list:
+        next_level = []
+        for n in nested_list:
+            if n.isInteger():
+                unweighted += n.getInteger()
+            else:
+                next_level += n.getList()
+        weighted += unweighted
+        nested_list = next_level
+    return weighted
 
-class Solution(object):
-    def nestedWeightSum(self, nestedls):
-        weighted, unweighted = 0, 0
-        while nestedls:
-            nextLevel = []
-            for ele in nestedls:
-                if not isinstance(ele, list):
-                    unweighted += ele
-                else:
-                    nextLevel += ele
-            weighted += unweighted
-            nestedls = nextLevel
-        return weighted
-slt = Solution()
-print(slt.nestedWeightSum([[1,1],2,[1,1]]))
-print(slt.nestedWeightSum([1,[4,[6]]]))
-
-
-##second
-class Solution(object):
-    def depthSumInverse(self, nestedls):
-        self.res = []
-        for ele in nestedls:
-            self.depthSumInverseHelper(ele, 0)
-        sum = 0
-        for i in reversed(range(len(self.res))):
-            sum += self.res[i] * (len(self.res) - i)
-        return sum
-    def depthSumInverseHelper(self, ls, depth):
-        if len(self.res) < depth + 1:
-            self.res.append(0)
-        if ls.isInteger():
-            self.res[depth] += ls.getInteger()
-        else:
-            for l in ls.getList():
-                self.depthSumInverseHelper(l, depth + 1)
+###recursion   
+def InversedepthSum2(nested_list):
+        res = []
+        for n in nested_list:
+            InversedepthSum2Recu(n, 0, res)
+        sumi = 0
+        for i in reversed(range(len(res))):
+            sumi += res[i] * (len(res) - i)
+        return sumi
+def InversedepthSum2Recu(ls, depth, res):
+    if len(res) < depth + 1:
+        res.append(0)
+    if ls.isInteger():
+        res[depth] += ls.getInteger()
+    else:
+        for l in ls.getList():
+            InversedepthSum2Recu(l, depth + 1)
