178afd3b67a823b6f348a68bbf1933b6cb7b18ec
Shanshan Li @JPM <shaniavina@gmail.com>
2018-11-15 11:54:45 -0500

    Update palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -6,11 +6,10 @@ class Solution(object):
         """
         if x < 0:
             return False
-        copy, reverse = x, 0
-        while copy:
-            reverse *= 10
-            reverse += copy % 10
-            copy /= 10
-        return x ==  reverse
+        x_copy, reverse = x, 0
+        while x_copy:
+            reverse = reverse * 10 + x_copy % 10
+            x_copy /= 10
+        return x == reverse
 
     
