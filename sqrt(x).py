0e1e27d8380c4178db4bb6ab65224a67aa632f73
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-17 17:23:29 -0500

    Update sqrt(x).py

--- sqrt(x).py
+++ sqrt(x).py
@@ -6,15 +6,14 @@ class Solution(object):
         """
         if x in (0, 1):
             return x
-        left, right = 1, x / 2   ####a half is okay
+        left, right = 1, x / 2   ####can not starting from 0 since we have it as nominator
         while left <= right:
             mid = left + (right - left) / 2   ####write in this way: overflow!
-            if mid > x / mid:      ###overflow!
+            if mid > x / mid:      
                 right = mid - 1
             else:
                 left = mid + 1
         return left - 1
-    
     
     
 #     // Newton's method
