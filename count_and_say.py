481ced5f69122a7c4de38d7dfe14a1f2254dc85f
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-17 15:49:49 -0500

    Update count_and_say.py

--- count_and_say.py
+++ count_and_say.py
@@ -1,21 +1,20 @@
-class Solution(object):
-    def countAndSay(self, n):
-        """
-        :type n: int
-        :rtype: str
-        """
-        seq = '1'
-        for i in range(n - 1):
-            seq = self.getNext(seq)   ####using a function and do it n times
-        return seq
-    def getNext(self, seq):
-        i, next_seq = 0, ''
-        while i < len(seq):
-            cnt = 1
-            while i < len(seq) - 1 and seq[i] == seq[i + 1]: ####len - 1
-                cnt += 1
-                i += 1
-            next_seq += str(cnt) + seq[i]
+def countAndSay(self, n):
+    """
+    :type n: int
+    :rtype: str
+    """
+    seq = '1'
+    for i in range(n - 1):
+        seq = self.getNext(seq)    ####using a function and do it n times
+    return seq
+def getNext(self, seq):
+    i, next_seq = 0, ''
+    while i < len(seq):  ####when the step is not necessaritly 1
+        cnt = 1
+        while i < len(seq) - 1 and seq[i] == seq[i + 1]:
+            cnt += 1
             i += 1
-        return next_seq
-            
+        next_seq += str(cnt) + seq[i]
+        i += 1
+    return next_seq
+        
