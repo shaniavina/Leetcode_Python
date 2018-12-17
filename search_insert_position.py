eac4dd9ee979ee60beb7d3346caf159394daf4b2
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-17 14:46:07 -0500

    Update search_insert_position.py

--- search_insert_position.py
+++ search_insert_position.py
@@ -5,15 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+        
         if not nums:
-            return None
+            return 0
         
         left, right = 0, len(nums) - 1
         while left <= right:      ###if < without =, may not get in the loop when there is only one element
@@ -23,13 +17,7 @@ class Solution(object):
             else:
                 left = mid + 1
         return left
-        
-        
-#         for i in range(len(nums)):
-#             if target <= nums[i]:
-#                 return i
-#         return len(nums)
-        
+      
     
     
         
