e234dc6019a8c38ba763868a6a36252fc73325b5
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-18 16:43:51 -0500

    Update merge_sorted_array.py

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -7,43 +7,6 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
-        i = 0
-        j = 0
-        ret = []
-        while i < m or j < n:
-            if j == n or (i < m and nums1[i] < nums2[j]):
-                ret.append(nums1[i])
-                i += 1
-            else:
-                ret.append(nums2[j])
-                j += 1
-        for i in range(len(ret)):
-            nums1[i] = ret[i]
-            
-###second           
-       class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
-        """
-#         if m == 0:
-#             nums1 = nums2
-       
-        # i, j = 0, 0
-        # while i < m and j < n:
-        #     if nums1[i + j] >= nums2[j]:
-        #         nums1 = [nums2[j]] + nums1
-        #         j += 1
-        #     else:
-        #         i += 1
-        # if j < n:
-        #     nums1 += nums2[j:]
-        
-        
         ####beautiful code   ####from the end
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
