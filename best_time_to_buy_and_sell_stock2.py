a982f9d41471fda799b0008c83d79dfcbb7d1093
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-19 15:24:33 -0500

    Update best_time_to_buy_and_sell_stock2.py

--- best_time_to_buy_and_sell_stock2.py
+++ best_time_to_buy_and_sell_stock2.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        if not prices:
+            return 0
         profit = 0
-        if len(prices) == 0:
-            return profit
         for i in range(len(prices) - 1):
-            profit += max(0, prices[i+1] - prices[i])
+            profit += max(0, prices[i + 1] - prices[i])
         return profit
         
 if __name__ == '__main__':
