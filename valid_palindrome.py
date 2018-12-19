4eb1b6790ca71cbce99c5475b5134d449b6a9fb4
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-19 15:55:09 -0500

    Update valid_palindrome.py

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-       
+        lst = list(char for char in s.lower() if char.isalnum())  ###charater functions: .isalnum() and .lower()
+        return lst == lst[::-1]
+        
+    ####second: comaprison one by one
+        lst = list(char for char in s.lower() if char.isalnum())
+        left, right = 0, len(lst) - 1
+        while left < right:
+            if lst[left] == lst[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
+        
