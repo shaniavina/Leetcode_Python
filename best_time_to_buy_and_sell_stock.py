626ae8fc65e66d9e7a9b63793c1b46e4a29dc9ed
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-19 15:22:05 -0500

    Update best_time_to_buy_and_sell_stock.py

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        maxProfit, min_price = 0, float("inf")
-        for price in prices:
-            min_price = min(min_price, price)
-            maxProfit = max(maxProfit, price - min_price)
-        return maxProfit
+        def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
 if __name__ == '__main__':
     print Solution().maxProfit([2,3,1,4,56,3,2])
