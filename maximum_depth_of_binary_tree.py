45cfd2e063a431653a0a71949d6bdfd64c5d5595
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-19 11:00:24 -0500

    Update maximum_depth_of_binary_tree.py

--- maximum_depth_of_binary_tree.py
+++ maximum_depth_of_binary_tree.py
@@ -13,7 +13,6 @@ class Solution(object):
         """
         if root is None:
             return 0
-      
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
 
 if __name__ == "__main__":
