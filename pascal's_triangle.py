3dcb7c56903f2b026818e8b224b1237c9a2c18c8
Shanshan Li @JPM <shaniavina@gmail.com>
2018-12-19 13:46:03 -0500

    Update pascal's_triangle.py

--- pascal's_triangle.py
+++ pascal's_triangle.py
@@ -8,7 +8,7 @@ class Solution(object):
         for i in range(numRows):
             result.append([])
             for j in range(i + 1):
-                if j in (0, i):     #####in these two 
+                if j in (0, i):     #####the frist and the last
                     result[i].append(1)
                 else:
                     result[i].append(result[i - 1][j - 1] + result[i - 1][j])
