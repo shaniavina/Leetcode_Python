570a66cf7a0b0afde6ab2feed6bde394fb51db8c
Shanshan Li @JPM <shaniavina@gmail.com>
2019-01-11 17:05:48 -0500

    Update intersection_of_two_linked_lists.py

--- intersection_of_two_linked_lists.py
+++ intersection_of_two_linked_lists.py
@@ -4,50 +4,6 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def move(self, head, steps):
-        node = head
-        for i in range(steps):
-            node = node.next
-        return node
-    
-    def getIntersectionNode(self, headA, headB):
-        """
-        :type head1, head1: ListNode
-        :rtype: ListNode
-        """
-        if headA == None or headB == None:
-            return None
-        
-        tailA = headA
-        countA = 1
-        while tailA.next != None:
-            tailA = tailA.next
-            countA += 1
-        
-        tailB = headB
-        countB = 1
-        while tailB.next != None:
-            tailB = tailB.next
-            countB += 1
-            
-        if tailA != tailB:
-            return None
-        else:
-            diff = abs(countA - countB)
-            curA = headA
-            curB = headB
-            if countA > countB:
-                curA = self.move(headA, diff)
-            else:
-                curB = self.move(headB, diff)
-            while curA != curB:
-                curA = curA.next
-                curB = curB.next
-        return curA
-    
-    
-    ############second solution, very very interesting
 
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
