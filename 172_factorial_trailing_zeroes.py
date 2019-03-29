636ffffd2669f1b1e24c02e208fd600487cb005c
Shanshan Li @JPM <shaniavina@gmail.com>
2019-03-29 10:45:26 -0400

    Update and rename factorial_trailing_zeroes.py to 172_factorial_trailing_zeroes.py

--- 172_factorial_trailing_zeroes.py
+++ 172_factorial_trailing_zeroes.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        result = 0
-        while n > 0:     ####while n:
-            result += n / 5
+        res = 0
+        while n > 0:
+            res += n / 5
             n /= 5
-        return result
+        return res
