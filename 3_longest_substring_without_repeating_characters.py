da828bcd7f3d42dc91972482608eb52e03dc7310
Shanshan Li @JPM <shaniavina@gmail.com>
2019-07-30 16:13:18 -0400

    Update 3_longest_substring_without_repeating_characters.py

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -6,11 +6,11 @@ class Solution(object):
         """
         if not s:
             return 0
-        
-        start = maxLength = 0
+        start = 0
+        maxLength = 0
         lookup = {}
         for i in range(len(s)):
-            if s[i] in lookup and start <= lookup[s[i]]:
+            if s[i] in lookup and start <= lookup[s[i]]: ## the last index position of the letter ('taamust')
                 start = lookup[s[i]] + 1
             else:
                 maxLength = max(maxLength, i - start + 1)
