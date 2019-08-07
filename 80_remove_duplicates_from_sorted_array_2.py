204e2a9ead2550baa07648ebdcec792e910b1beb
Shanshan Li @JPM <shaniavina@gmail.com>
2019-08-07 14:00:23 -0400

    Update and rename remove_duplicates_from_sorted_array_2.py to 80_remove_duplicates_from_sorted_array_2.py

--- 80_remove_duplicates_from_sorted_array_2.py
+++ 80_remove_duplicates_from_sorted_array_2.py
@@ -12,21 +12,8 @@ class Solution(object):
                 same = nums[i] == nums[last]
                 last += 1
                 nums[last] = nums[i]
-            i += 1
+            i += 1  ### why for not working while while works?
         return last + 1
         
 
-        
-        class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        tail = 0
-        for num in nums:
-            if tail < 2 or num != nums[tail - 1] or num != nums[tail - 2]:
-                nums[tail] = num
-                tail += 1
-        return tail
+      
