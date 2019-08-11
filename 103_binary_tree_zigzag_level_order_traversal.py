ad089b26c0c7c0a634504415f0a47e5711fd232b
Shanshan Li @JPM <shaniavina@gmail.com>
2019-08-11 16:18:21 -0400

    Update and rename binary_tree_zigzag_level_order_traversal.py to 103_binary_tree_zigzag_level_order_traversal.py

---
+++ 103_binary_tree_zigzag_level_order_traversal.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+#         # recursion
+#         res = []
+#         self.levelOrderRecu(root, 0, res)
+#         return res
+#     def levelOrderRecu(self, node, level, res):
+#         if not node:
+#             return
+#         if len(res) == level:
+#             res.append([node.val])
+#         elif level % 2:
+#             res[level].insert(0, node.val)
+#         else:
+#             res[level].append(node.val)
+#         self.levelOrderRecu(node.left, level + 1, res)
+#         self.levelOrderRecu(node.right, level + 1, res)
+    
+        
+        ### stack, BFS
+        if not root:
+            return []
+        q = [root]
+        res = []
+        while q:
+            next_q = []
+            tmp = []
+            for node in q:
+                tmp.append(node.val)
+                if node.left:
+                    next_q.append(node.left)
+                if node.right:
+                    next_q.append(node.right)
+            if len(res) % 2:
+                res.append(tmp[::-1])
+            else:
+                res.append(tmp)
+            q = next_q
+        return res
