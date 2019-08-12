d35f47ccf1e50d71127ae94b9e50441fb434fd1b
Shanshan Li @JPM <shaniavina@gmail.com>
2019-08-11 23:56:27 -0400

    Update 14_longest_common_prefix.py

--- 14_longest_common_prefix.py
+++ 14_longest_common_prefix.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if not strs:  ###can not put in 'if strs is None:'
+        if not strs:
             return ''
-        
         for i in range(len(strs[0])):
             for string in strs[1:]:
                 if i >= len(string) or string[i] != strs[0][i]:
