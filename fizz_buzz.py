4ea0a1f5e61e8172eefd2dd3a65ac5ab83859f9e
Shanshan Li @JPM <shaniavina@gmail.com>
2019-08-28 23:10:16 -0400

    Update and rename fizz_buzz.py to 412_fizz_buzz.py

--- fizz_buzz.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        
-        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n + 1)]
