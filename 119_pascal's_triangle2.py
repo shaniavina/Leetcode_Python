b824c3fd07ded0d2ad64025777c2f8dffcfa2e17
Shanshan Li @JPM <shaniavina@gmail.com>
2019-09-10 23:32:18 -0400

    Update 119_pascal's_triangle2.py

--- 119_pascal's_triangle2.py
+++ 119_pascal's_triangle2.py
@@ -4,29 +4,9 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        if rowIndex == 0:
-            return [1]
-        elif rowIndex == 1:
-            return [1, 1]
-        else:
-            currentRow = 2
-            previous = [1, 1]
-            while currentRow <= rowIndex:
-                current = [1 for i in range(rowIndex + 1)]
-                for i in range(1, currentRow):
-                    current[i] = previous[i - 1] + previous[i]
-                previous = current
-                currentRow += 1
-        return current
-    
-#####second 
-class Solution(object):
-    def getRow(self, rowIndex):
-        """
-        :type rowIndex: int
-        :rtype: List[int]
-        """
-        result = [1]    ####local variable 'result' referenced before assignment
+        row = [1]
         for i in range(1, rowIndex + 1):
-            result = [1] + [result[j - 1] + result[j] for j in range(1, i)] + [1]
-        return result
+            row = [1] + [row[k] + row[k + 1] for k in range(len(row) - 1)] + [1]
+        return row
+        
+       
