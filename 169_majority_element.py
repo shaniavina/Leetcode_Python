9a02ee4ef3fd3940bfcaf07d6a6181ccec676a7b
Shanshan Li @JPM <shaniavina@gmail.com>
2019-09-10 23:47:37 -0400

    Update 169_majority_element.py

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -18,3 +18,5 @@ class Solution(object):
 
         #### NOTICE that the majority element always exist in the array,so that the middle always is the answer
         return sorted(num)[len(num)/2]
+
+    
