9393ae5f942e85ce40f3677a5faee78161709801
Shanshan Li @JPM <shaniavina@gmail.com>
2019-09-10 23:12:45 -0400

    Update 66_plus_one.py

--- 66_plus_one.py
+++ 66_plus_one.py
@@ -15,3 +15,18 @@ class Solution(object):
             
             
            
+##### second
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if len(digits) == 1 and digits[0] == 9:
+            return [1, 0]
+        if digits[-1] != 9:
+            digits[-1] += 1
+        else:
+            digits[-1] = 0
+            digits[:-1] = self.plusOne(digits[:-1])
+        return digits
