95eaa2f134c70f2f6939bf2f870b6627e93964ee
Shanshan Li @JPM <shaniavina@gmail.com>
2019-09-15 20:05:44 -0400

    Update and rename reverse_string2.py to 541_reverse_string2.py

--- 541_reverse_string2.py
+++ 541_reverse_string2.py
@@ -7,5 +7,5 @@ class Solution(object):
         """
         s = list(s)
         for i in range(0, len(s), 2 * k):
-            s[i:i + k] = reversed(s[i: i + k])
+            s[i: i + k] = reversed(s[i: i + k])
         return ''.join(s)
