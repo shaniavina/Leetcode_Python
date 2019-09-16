27977b1a1240f18d9feae48fa3b69ef9b83c6664
Shanshan Li @JPM <shaniavina@gmail.com>
2019-09-15 20:13:48 -0400

    Update and rename reverse_linked_list.py to 206_reverse_linked_list.py

--- 206_reverse_linked_list.py
+++ 206_reverse_linked_list.py
@@ -1,21 +1,6 @@
 # Definition for singly-linked list.
 # class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        ####my edition: iteratively
-        dummy = ListNode('-inf')
-        while head:
-            head.next, dummy.next, head = dummy.next, head, head.next
-            
-        return dummy.next
+#     def __init__(self, x)
 
 # Time:  O(n)
 # Space: O(n)
