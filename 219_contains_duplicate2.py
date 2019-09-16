dfde60fc5c11f94804152e093b5865d3304190f9
Shanshan Li @JPM <shaniavina@gmail.com>
2019-09-15 20:34:56 -0400

    Update and rename contains_duplicate2.py to 219_contains_duplicate2.py

--- 219_contains_duplicate2.py
+++ 219_contains_duplicate2.py
@@ -5,15 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        num_pos_map = {}
-        for i in range(len(nums)):
-            if (nums[i] in num_pos_map) and (i - num_pos_map[nums[i]] <= k):
-                return True
-            else:
-                num_pos_map[nums[i]] = i
-           
-        return False
-                
+        
 ####mine
 lookup = {}
         for i, num in enumerate(nums):
