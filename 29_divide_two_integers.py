f8e760524834321b75c2c511659c85e53005ddcf
Shanshan Li @JPM <shaniavina@gmail.com>
2019-09-16 23:51:37 -0400

    Update 29_divide_two_integers.py

--- 29_divide_two_integers.py
+++ 29_divide_two_integers.py
@@ -1,19 +1,25 @@
-class Solution:
-# @return an integer
-def divide(self, dividend, divisor):
-    positive = (dividend < 0) is (divisor < 0)
-    dividend, divisor = abs(dividend), abs(divisor)
-    res = 0
-    while dividend >= divisor:
-        temp, i = divisor, 1
-        while dividend >= temp:
-            dividend -= temp
-            res += i
-            i <<= 1
-            temp <<= 1
-    if not positive:
-        res = -res
-    return min(max(-2147483648, res), 2147483647)
-    
-    
-    
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        # keep substracting the new divisor div from the remaining left and then doubling div. if left < div, start from the orginal divisor.
+        neg = (dividend < 0) != (divisor < 0)
+        dividend = left = abs(dividend)
+        divisor = div = abs(divisor)
+        Q = 1
+        res = 0
+        while left >= div:
+            left -= div
+            res += Q
+            div += div
+            Q += Q
+            if left < div:
+                div = divisor
+                Q = 1
+        if neg:
+            return max(-2 ** 31, -res)
+        else:
+            return min(2 ** 31 - 1, res)
