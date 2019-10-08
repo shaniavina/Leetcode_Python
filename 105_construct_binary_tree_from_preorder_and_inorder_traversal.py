fe6f086bc7db6cd704a8b1f20efa2319cc434b9b
Shanshan Li @JPM <shaniavina@gmail.com>
2019-10-07 23:00:15 -0400

    Update and rename construct_binary_tree_from_preorder_and_inorder_traversal.py to 105_construct_binary_tree_from_preorder_and_inorder_traversal.py

--- 105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ 105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -25,3 +25,20 @@ class Solution(object):
         node.left = self.buildTreeRecu(lookup, preorder, inorder, pre_start + 1, in_start, i)
         node.right = self.buildTreeRecu(lookup, preorder, inorder, pre_start + 1 + i - in_start, i + 1, in_end)
         return node
+
+    
+    ### second
+    class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if inorder:
+            ind = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder[0: ind])
+            root.right = self.buildTree(preorder, inorder[ind + 1: ])
+            return root
+            
