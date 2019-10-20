50a3e9ba7bd36ce8e96e5a5e2ea60933f39f888a
Shanshan Li @JPM <shaniavina@gmail.com>
2019-10-20 15:28:36 -0400

    Update palindrom_partitioning.py

--- palindrom_partitioning.py
+++ palindrom_partitioning.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
-        self.res = []
-        self.dfs(s, [])
-        return self.res
-
-    def dfs(self, s, path):
+        #time complexity: n * 2 ^ n
+        # for each interval, cut or not cut --> 2 ** (n - 1)
+        # for each case, to check isPal --> n
+        res = []
+        self.dfs(s, [], res)
+        return res
+    def dfs(self, s, path, res):
         if not s:
-            self.res.append(list(path))
+            res.append(path)
+            return
         for i in range(1, len(s) + 1):
             if self.isPal(s[:i]):
-                self.dfs(s[i:], path + [s[:i]])   ###can only concatenate list; [[]]
+                self.dfs(s[i:], path + [s[:i]], res)
+                
     def isPal(self, s):
         return s == s[::-1]
-    
- 
