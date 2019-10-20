0e0dfde7c524b769e35420dff1b2fe0bdf783aa3
Shanshan Li @JPM <shaniavina@gmail.com>
2019-10-20 18:08:38 -0400

    Update and rename kth_largest_element_in_an_array.py to 215_kth_largest_element_in_an_array.py

---
+++ 215_kth_largest_element_in_an_array.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums = [-num for num in nums]
+        heapq.heapify(nums)
+        res = float('inf')
+        for _ in range(k):
+            res = heapq.heappop(nums)
+        return -res
