63fe2020c06dd8dc21ba29c89cd713633bbbdc25
Shanshan Li @JPM <shaniavina@gmail.com>
2019-10-20 17:20:58 -0400

    Update and rename power_of_four.py to 342_power_of_four.py

--- 342_power_of_four.py
+++ 342_power_of_four.py
@@ -9,6 +9,7 @@ class Solution(object):
         while num % 4 == 0:
             num /= 4
         return num == 1
+        
         #######second solution
         ###(0x55555555) <==> 1010101010101010101010101010101
         return num > 0 and num & (num - 1) == 0 and num & 0x55555555 == num
