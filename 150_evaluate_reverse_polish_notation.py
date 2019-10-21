d8881b7787c6ecf09156e90ce57d4a2c1571b108
Shanshan Li @JPM <shaniavina@gmail.com>
2019-10-20 21:44:39 -0400

    Update and rename evaluate_reverse_polish_notation.py to 150_evaluate_reverse_polish_notation.py

---
+++ 150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for t in tokens:
+            if t not in ['+', '-', '*', '/']:
+                stack.append(int(t))
+            else:
+                r, l = stack.pop(), stack.pop()
+                if t == '+':
+                    stack.append(r + l)
+                elif t == '-':
+                    stack.append(l - r)
+                elif t == '*':
+                    stack.append(l * r)
+                else:
+                    stack.append(int(float(l) / float(r)))
+        return stack.pop()
