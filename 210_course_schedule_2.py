f82acb089033e231950e52cacb1628c8f0fcbdff
Shanshan Li @JPM <shaniavina@gmail.com>
2019-10-20 22:25:29 -0400

    Update and rename course_schedule_2.py to 210_course_schedule_2.py

---
+++ 210_course_schedule_2.py
@@ -0,0 +1,24 @@
+import collections
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        dic = collections.defaultdict(set)
+        neigh = collections.defaultdict(set)
+        for i, j in prerequisites:
+            dic[i].add(j)
+            neigh[j].add(i)
+        stack = [i for i in range(numCourses) if not dic[i]]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node)
+            for i in neigh[node]:
+                dic[i].remove(node)
+                if not dic[i]:
+                    stack.append(i)
+            dic.pop(node)
+        return res if not dic else []
