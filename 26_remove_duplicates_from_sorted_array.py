2fde3b454c6d74297b68d325f4b5e275febb54b9
Shanshan Li @JPM <shaniavina@gmail.com>
2020-07-22 15:13:04 -0400

    Update 26_remove_duplicates_from_sorted_array.py

--- 26_remove_duplicates_from_sorted_array.py
+++ 26_remove_duplicates_from_sorted_array.py
@@ -4,11 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # two pointers
+        # space: O(1)
+        # time: O(n)
+        
         if not nums:
             return 0
-        i, last = 1, 0  ###two pointers
-        for i in range(len(nums)):
-            if nums[i] != nums[last]:
-                last += 1
-                nums[last] = nums[i]
-        return last + 1
+        slow = 0 
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow + 1
+    
