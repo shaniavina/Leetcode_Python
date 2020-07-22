a2c69c2a51bc227f63f512344be5cb8803972f62
Shanshan Li @JPM <shaniavina@gmail.com>
2020-07-22 15:26:37 -0400

    Update 53_maximum_subarray.py

--- 53_maximum_subarray.py
+++ 53_maximum_subarray.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # dynamic programming
+        # time: O(n)
+        # space: O(1)
         if not nums:
             return 0
-        # if max(nums) < 0:
-        #     return max(nums)
         global_max, local_max = float('-inf'), 0
         for num in nums:
             local_max = max(local_max + num, num)
-            global_max = max(local_max, global_max)
+            global_max = max(global_max, local_max)
         return global_max
-            
-            
-            
+    
