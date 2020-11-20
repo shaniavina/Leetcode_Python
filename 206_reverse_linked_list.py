80a6d486beb832b9b75a390e3f258d3f027dd12f
Shanshan Li @JPM <shaniavina@gmail.com>
2020-11-20 17:03:15 -0500

    Update 206_reverse_linked_list.py

--- 206_reverse_linked_list.py
+++ 206_reverse_linked_list.py
@@ -1,27 +1,26 @@
 # Definition for singly-linked list.
 # class ListNode(object):
-#     def __init__(self, x)
-
-# Time:  O(n)
-# Space: O(n)
-# Recursive solution.  
-
-class Solution:
-    
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
     def reverseList(self, head):
-        return self.doReverse(head, None)
-    def doReverse(self, head, newHead):
-        if head is None:
-            return newHead
-        next = head.next
-        head.next = newHead
-        return self.doReverse(next, head)
-
-
-if __name__ == "__main__":
-    head = ListNode(1)
-    head.next = ListNode(2)
-    head.next.next = ListNode(3)
-    head.next.next.next = ListNode(4)
-    head.next.next.next.next = ListNode(5)
-    print Solution2().reverseList(head)
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+#         return self.doReverse(head, None)
+#     def doReverse(self, head, newHead):
+#         if head is None:
+#             return newHead
+#         next = head.next
+#         head.next = newHead
+#         return self.doReverse(next, head)
+    
+        cur, pre = head, None
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
