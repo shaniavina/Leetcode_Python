936b691ac179f280d2db51be9e0eb42e0dc7b865
Shanshan Li @JPM <shaniavina@gmail.com>
2022-08-30 23:09:18 -0400

    Update 412_fizz_buzz.py

--- 412_fizz_buzz.py
+++ 412_fizz_buzz.py
@@ -1,19 +1,19 @@
-class Solution(object):
-    def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        res = []
-        for i in range(1, n + 1): 
-            if i % 15 == 0:
-                res.append('FizzBuzz')
-            elif i % 3 == 0:
-                res.append('Fizz')
-            elif i % 5 == 0:
-                res.append('Buzz')
-            else:
-                res.append(str(i))
-        return res
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for num in range(1, n + 1):
+            divisible_by_3 = (num % 3 == 0)
+            divisible_by_5 = (num % 5 == 0)
             
-                
+            if divisible_by_3 and divisible_by_5:
+                ans.append('FizzBuzz')
+            elif divisible_by_3:
+                ans.append('Fizz')
+            elif divisible_by_5:
+                ans.append('Buzz')
+            else:
+                ans.append(str(num))
+        return ans
+
+#Time Complexity: O(N)
+#Space Complexity: O(1)
